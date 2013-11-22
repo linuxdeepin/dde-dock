7fa71938ada8c389475c5a480ec7a022f8eb5637
jouyouyun <jouyouwen717@gmail.com>
2013-11-22 17:39:31 +0800

    add shutdown

---
+++ shutdown/shutdown.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"dbus-gen/gsession"
+	"dbus-gen/upower"
+	"dlib/dbus"
+)
+
+type DShutdown struct{}
+
+var (
+	dShut  *gsession.SessionManager
+	dPower *upower.Upower
+)
+
+func (shutdown *DShutdown) GetDBusInfo() dbus.DBusInfo {
+	return dbus.DBusInfo{
+		"com.deepin.daemon.DShutdown",
+		"/com/deepin/daemon/DShutdown",
+		"com.deepin.daemon.DShutdown",
+	}
+}
+
+func (shutdown *DShutdown) Logout() {
+	dShut.Logout(1)
+}
+
+func (shutdown *DShutdown) Shutdown() bool {
+	if !dShut.CanShutdown() {
+		return false
+	}
+
+	dShut.RequestShutdown()
+	return true
+}
+
+func (shutdown *DShutdown) Reboot() {
+	dShut.RequestReboot()
+}
+
+func (shutdown *DShutdown) Suspend() {
+	dPower.Suspend()
+}
+
+func (shutdown *DShutdown) Hibernate() {
+	dPower.Hibernate()
+}
+
+func NewShutdown() *DShutdown {
+	shutdown := DShutdown{}
+	return &shutdown
+}
+
+func main() {
+	dShut = gsession.GetSessionManager("/org/gnome/SessionManager")
+	dPower = upower.GetUpower("/org/freedesktop/UPower")
+
+	shutdown := NewShutdown()
+	dbus.InstallOnSession(shutdown)
+	select {}
+}
