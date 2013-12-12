9581f3d93c2ad9d14f73c108ab0f904c354eba53
jouyouyun <jouyouwen717@gmail.com>
2013-12-12 13:29:22 +0800

    add blur picture

---
+++ blur-picture/blur-pict.h
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2011 ~ 2013 Deepin, Inc.
+ *               2011 ~ 2013 jouyouyun
+ *
+ * Author:      jouyouyun <jouyouwen717@gmail.com>
+ * Maintainer:  jouyouyun <jouyouwen717@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef __BLUR_PICT_H__
+#define __BLUR_PICT_H__
+
+int generate_blur_pict (const char *src_path, const char *dest_path, 
+		double sigma, long numsteps);
+int blur_pict_is_valid (const char *src_path, const char *dest_path);
+
+#endif
