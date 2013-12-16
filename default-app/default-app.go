120605723836e097b7ca673a957f6584c096e6c5
jouyouyun <jouyouwen717@gmail.com>
2013-12-16 15:58:04 +0800

    default-app: add terminal

--- default-app/default-app.go
+++ default-app/default-app.go
@@ -2,34 +2,64 @@ package main
 
 import (
 	"dlib/gio-2.0"
+	"dlib/glib-2.0"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 type DefaultApps struct{}
 
 type AppInfo struct {
-	ID          string
-	Name        string
-	DisplayName string
-	Desc        string
-	Exec        string
-	Command     string
+	ID   string
+	Name string
+	Exec string
 }
+
+const (
+	_TERMINAL_SCHEMA = "org.gnome.desktop.default-applications.terminal"
+	_DESKTOP_PATH    = "/usr/share/applications/"
+
+	_TERMINAL_EMULATOR   = "TerminalEmulator"
+	_DESKTOP_ENTRY       = "Desktop Entry"
+	_X_TERMINAL_EMULATOR = "x-terminal-emulator"
+	_CATEGORY            = "Categories"
+	_EXEC                = "Exec"
+)
+
+var (
+	_TerminalBlacklist = []string{"guake"}
+
+	_TerminalGSettings = gio.NewSettings(_TERMINAL_SCHEMA)
+)
 
 func NewDAppInfo(gioApp *gio.AppInfo) *AppInfo {
 	dappInfo := AppInfo{}
 
 	dappInfo.ID = gioApp.GetId()
-	dappInfo.Name = gioApp.GetName()
-	dappInfo.DisplayName = gioApp.GetDisplayName()
-	dappInfo.Desc = gioApp.GetDescription()
+	dappInfo.Name = gioApp.GetDisplayName()
 	dappInfo.Exec = gioApp.GetExecutable()
-	dappInfo.Command = gioApp.GetCommandline()
 	return &dappInfo
 }
 
 func (dapp *DefaultApps) AppsListViaType(typeName string) []*AppInfo {
 	var defaultAppsList []*AppInfo
+
+	if typeName == "terminal" {
+		lists := GetTerminalList()
+		if lists == nil {
+			return nil
+		}
+
+		for _, v := range lists {
+			defaultAppsList = append(defaultAppsList,
+				NewAppInfoByID(_DESKTOP_PATH+v))
+		}
+
+		return defaultAppsList
+	}
+
 	gioAppsList := gio.AppInfoGetAllForType(typeName)
 	for _, gioApp := range gioAppsList {
 		defaultAppsList = append(defaultAppsList, NewDAppInfo(gioApp))
@@ -38,11 +68,33 @@ func (dapp *DefaultApps) AppsListViaType(typeName string) []*AppInfo {
 }
 
 func (dapp *DefaultApps) DefaultAppViaType(typeName string) *AppInfo {
+	if typeName == "terminal" {
+		exec := _TerminalGSettings.GetString("exec")
+		terminalApps := dapp.AppsListViaType(typeName)
+
+		for _, v := range terminalApps {
+			if exec == v.Exec {
+				return v
+			}
+		}
+
+		return nil
+	}
+
 	gioApp := gio.AppInfoGetDefaultForType(typeName, false)
 	return NewDAppInfo(gioApp)
 }
 
 func (dapp *DefaultApps) SetDefaultAppViaType(typeName, appID string) bool {
+	if typeName == "terminal" {
+		appInfo := NewAppInfoByID(_DESKTOP_PATH + appID)
+		if appInfo == nil {
+			return false
+		}
+
+		return _TerminalGSettings.SetString("exec", appInfo.Exec)
+	}
+
 	gio.AppInfoResetTypeAssociations(typeName)
 	gioAppsList := gio.AppInfoGetAllForType(typeName)
 	for _, gioApp := range gioAppsList {
@@ -57,4 +109,113 @@ func (dapp *DefaultApps) SetDefaultAppViaType(typeName, appID string) bool {
 	}
 
 	return true
+}
+
+func NewAppInfoByID(id string) *AppInfo {
+	appInfo := &AppInfo{}
+	keyFile := glib.NewKeyFile()
+	lang := GetLocalLang()
+
+	_, err1 := keyFile.LoadFromFile(id, glib.KeyFileFlagsNone)
+	if err1 != nil {
+		fmt.Println("Load File Failed:", err1)
+		return nil
+	}
+
+	name, err := keyFile.GetString(_DESKTOP_ENTRY, "Name["+lang+"]")
+	if err != nil {
+		name, err = keyFile.GetString(_DESKTOP_ENTRY, "Name")
+	}
+
+	exec, err2 := keyFile.GetString(_DESKTOP_ENTRY, _EXEC)
+	if err2 != nil {
+		fmt.Println("Get Exec Failed:", err2)
+		return nil
+	}
+
+	appInfo.ID = id
+	appInfo.Name = name
+	appInfo.Exec = exec
+
+	return appInfo
+}
+
+func GetLocalLang() string {
+	langStr := os.Getenv("LANG")
+	array := strings.Split(langStr, ".")
+	return array[0]
+}
+
+func GetTerminalList() []string {
+	terminalList := []string{}
+	entryList, err := GetDesktopEntryList()
+	if err != nil {
+		fmt.Println("Get Desktop Entry List Failed")
+		return nil
+	}
+
+	for _, v := range entryList {
+		if IsTerminalEmulator(_DESKTOP_PATH + v) {
+			terminalList = append(terminalList, v)
+		}
+	}
+
+	return terminalList
+}
+
+func IsTerminalEmulator(fileName string) bool {
+	keyFile := glib.NewKeyFile()
+	_, err := keyFile.LoadFromFile(fileName, glib.KeyFileFlagsNone)
+	if err != nil {
+		fmt.Println("KeyFile Load File Failed:", err)
+		return false
+	}
+
+	categories, err := keyFile.GetString(_DESKTOP_ENTRY, _CATEGORY)
+	if err != nil {
+		fmt.Println("KeyFile Get String Failed:", err)
+		return false
+	}
+
+	if strings.Contains(categories, _TERMINAL_EMULATOR) {
+		execName, err := keyFile.GetString(_DESKTOP_ENTRY, _EXEC)
+		if err != nil {
+			fmt.Println("KeyFile Get String Failed:", err)
+			return false
+		}
+
+		if strings.Contains(execName, _X_TERMINAL_EMULATOR) {
+			return false
+		}
+
+		for _, v := range _TerminalBlacklist {
+			if strings.Contains(execName, v) {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	return false
+}
+
+func GetDesktopEntryList() ([]string, error) {
+	entryList := []string{}
+
+	desktops, err := ioutil.ReadDir(_DESKTOP_PATH)
+	if err != nil {
+		fmt.Println("Read Dir Failed:", err)
+		return nil, err
+	}
+
+	for _, fileInfo := range desktops {
+		if fileInfo.IsDir() {
+			continue
+		}
+
+		entryList = append(entryList, fileInfo.Name())
+	}
+
+	return entryList, nil
 }
--- default-app/default-media.go
+++ default-app/default-media.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"dlib/dbus"
 	"dlib/dbus/property"
 	"dlib/gio-2.0"
 )
@@ -11,61 +10,26 @@ var (
 )
 
 type MediaMount struct {
-	AllowAutoMount     bool
+	AutoMountOpen      *property.GSettingsBoolProperty `access:"readwrite"`
 	TypeIgnoreList     *property.GSettingsStrvProperty `access:"readwrite"`
 	TypeOpenFolderList *property.GSettingsStrvProperty `access:"readwrite"`
 	TypeExecList       *property.GSettingsStrvProperty `access:"readwrite"`
 }
 
-func (media *MediaMount) SetAllowAutoRun(allow bool) {
-	_mediaGSettings.SetBoolean("automount", allow)
-	_mediaGSettings.SetBoolean("automount-open", allow)
-}
-
 func NewMediaMount() *MediaMount {
 	media := &MediaMount{}
-	media.AllowAutoMount = GetAllowAutoMount()
-	media.TypeIgnoreList = property.NewGSettingsStrvProperty(media,
-		"TypeIgnoreList", _mediaGSettings,
-		"autorun-x-content-ignore")
-	media.TypeOpenFolderList = property.NewGSettingsStrvProperty(media,
-		"TypeOpenFolderList", _mediaGSettings,
-		"autorun-x-content-open-folder")
-	media.TypeExecList = property.NewGSettingsStrvProperty(media,
-		"TypeExecList", _mediaGSettings,
-		"autorun-x-content-start-app")
-
-	ListenGSettingsChanged(media)
+	media.AutoMountOpen = property.NewGSettingsBoolProperty(
+		media, "AutoMountOpen",
+		_mediaGSettings, "automount-open")
+	media.TypeIgnoreList = property.NewGSettingsStrvProperty(
+		media, "TypeIgnoreList",
+		_mediaGSettings, "autorun-x-content-ignore")
+	media.TypeOpenFolderList = property.NewGSettingsStrvProperty(
+		media, "TypeOpenFolderList",
+		_mediaGSettings, "autorun-x-content-open-folder")
+	media.TypeExecList = property.NewGSettingsStrvProperty(
+		media, "TypeExecList",
+		_mediaGSettings, "autorun-x-content-start-app")
 
 	return media
-}
-
-func GetAllowAutoMount() bool {
-	if GetAutoMount() && GetAutoMountOpen() {
-		return true
-	}
-
-	return false
-}
-
-func GetAutoMount() bool {
-	return _mediaGSettings.GetBoolean("automount")
-}
-
-func GetAutoMountOpen() bool {
-	return _mediaGSettings.GetBoolean("automount-open")
-}
-
-func ListenGSettingsChanged(media *MediaMount) {
-	_mediaGSettings.Connect("changed::automount",
-		func(s *gio.Settings, name string) {
-			media.AllowAutoMount = GetAllowAutoMount()
-			dbus.NotifyChange(media, "AllowAutoMount")
-		})
-
-	_mediaGSettings.Connect("changed::automount-open",
-		func(s *gio.Settings, name string) {
-			media.AllowAutoMount = GetAllowAutoMount()
-			dbus.NotifyChange(media, "AllowAutoMount")
-		})
 }
