bd012f4c6e1cc0d707b5ddaf44a46a3f0402dfba
jouyouyun <jouyouwen717@gmail.com>
2014-02-09 10:47:04 +0800

    mounts: get disk cap

---
+++ mounts/mounts-caps.go
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2011 ~ 2013 Deepin, Inc.
+ *               2011 ~ 2013 jouyouyun
+ *
+ * Author:      jouyouyun <jouyouwen717@gmail.com>
+ * Maintainer:  jouyouyun <jouyouwen717@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ **/
+
+package main
+
+import (
+	"dlib/logger"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+const (
+	CMD_DF = "/bin/df"
+	ARG_DF = "-h"
+)
+
+func getDiskCap(path string) (string, string) {
+	bytes, err := exec.Command(CMD_DF, ARG_DF).Output()
+	if err != nil {
+		logger.Println("Exec 'df -h' failed:", err)
+		panic(err)
+	}
+	fmt.Println("path:", path)
+
+	outStrs := strings.Split(string(bytes), "\n")
+	for _, str := range outStrs {
+		array := strings.Split(str, " ")
+		rets := delSpaceElment(array)
+		l := len(rets)
+		if l <= 2 {
+			break
+		}
+
+		if array[0] == path {
+			fmt.Println("Total:", rets[1])
+			fmt.Println("Used:", rets[2])
+			return rets[1], rets[2]
+		}
+	}
+
+	return "", ""
+}
+
+func delSpaceElment(strs []string) []string {
+	rets := []string{}
+
+	for _, v := range strs {
+		if len(v) > 0 {
+			rets = append(rets, v)
+		}
+	}
+
+	return rets
+}
