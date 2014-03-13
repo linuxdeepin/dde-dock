0e0ed377b6fd30a85cc8b30fb793027c2d0ef729
snyh <snyh@snyh.org>
2014-03-13 22:36:26 +0800

    dock-apps-builder: add test code

--- dock-apps-builder/entry.go
+++ dock-apps-builder/entry.go
@@ -1,17 +1,21 @@
 package main
 
 import "dlib/dbus"
-import "fmt"
-import "os"
+import "dlib/gio-2.0"
+import "crypto/md5"
+import "encoding/hex"
+
+//import "strings"
 
 type Rectangle struct {
 	X, Y          int16
 	Width, Height uint16
 }
 
-type Entry struct {
-	ID   string `dmusic`
-	Type string `applet/other`
+type DesktopEntry struct {
+	core *gio.DesktopAppInfo
+	ID   string
+	Type string
 
 	Tooltip string
 	Icon    string
@@ -22,24 +26,46 @@ type Entry struct {
 	Allocation         Rectangle
 }
 
-func (*Entry) QuickWindow(x, y int32)              {}
-func (*Entry) ContextMenu(x, y int32)              {}
-func (*Entry) Activate(x, y int32)                 {}
-func (*Entry) SecondaryActivate(x, y int32)        {}
-func (*Entry) OnDragEnter(x, y int32, data string) {}
-func (*Entry) OnDragLeave(x, y int32, data string) {}
-func (*Entry) OnDragOver(x, y int32, data string)  {}
-func (*Entry) OnDragDrop(x, y int32, data string)  {}
+func NewDesktopEntry(id string) *DesktopEntry {
+	e := &DesktopEntry{}
+	e.core = gio.NewDesktopAppInfo(id)
+	if e.core == nil {
+		return nil
+	}
+	e.Tooltip = e.core.GetName()
+	e.ID = e.core.GetId()
+	e.Icon = e.core.GetIcon().ToString()
+	return e
+}
 
-func (*Entry) GetDBusInfo() dbus.DBusInfo {
+func (e *DesktopEntry) QuickWindow(x, y int32) {}
+func (e *DesktopEntry) ContextMenu(x, y int32) {}
+func (e *DesktopEntry) Activate(x, y int32) {
+	e.core.Launch(nil, nil)
+}
+func (e *DesktopEntry) SecondaryActivate(x, y int32)        {}
+func (e *DesktopEntry) OnDragEnter(x, y int32, data string) {}
+func (e *DesktopEntry) OnDragLeave(x, y int32, data string) {}
+func (e *DesktopEntry) OnDragOver(x, y int32, data string)  {}
+func (e *DesktopEntry) OnDragDrop(x, y int32, data string)  {}
+
+func (e *DesktopEntry) GetDBusInfo() dbus.DBusInfo {
+	hasher := md5.New()
+	hasher.Write([]byte(e.ID))
+	// DBusObjectPath can't be start with digital number
+	id := "d" + hex.EncodeToString(hasher.Sum(nil))
 	return dbus.DBusInfo{
-		fmt.Sprintf("dde.dock.entry.app.chrome-%d", os.Getpid()),
-		"/dde/dock/entry/v1",
+		"dde.dock.entry." + id,
+		"/dde/dock/entry/v1/" + id,
 		"dde.dock.Entry",
 	}
 }
 
 func main() {
-	dbus.InstallOnSession(&Entry{Allocation: Rectangle{0, 0, 300, 400}})
+	for _, id := range loadAll() {
+		if e := NewDesktopEntry(id + ".desktop"); e != nil {
+			dbus.InstallOnSession(e)
+		}
+	}
 	dbus.Wait()
 }
---
+++ dock-apps-builder/load_desktops.go
@@ -0,0 +1,13 @@
+package main
+
+func loadAll() []string {
+	return []string{
+		"libreoffice-impress",
+		"gnome-system-monitor",
+		"google-chrome",
+		"d-feet",
+		"nautilus",
+		"deepin-music-player",
+		"deepin-terminal",
+	}
+}
