1228b582757ad2dc73cd40910841d62c7be46b93
Xu FaSheng <fasheng.xu@gmail.com>
2014-03-31 13:44:09 +0800

    network: add nm_autogen

---
+++ network/nm_autogen/template.go
@@ -0,0 +1,74 @@
+package main
+
+// get key type
+const tplGetKeyType = `
+// Get key type
+func get{{.FieldName | ToFieldFuncBaseName}}KeyType(key string) (t ktype) {
+	switch key {
+	default:
+		t = ktypeUnknown{{range .Keys}}
+	case {{.Name}}:
+		t = {{.Type}}{{end}}
+	}
+	return
+}
+`
+
+// get json value generally
+const tplGeneralGetterJSON = `
+// Get JSON value generally
+func generalGet{{.FieldName | ToFieldFuncBaseName}}KeyJSON(data _ConnectionData, key string) (value string) {
+	switch key {
+	default:
+		LOGGER.Error("generalGet{{.FieldName | ToFieldFuncBaseName}}KeyJSON: invalide key", key){{range .Keys}}
+	case {{.Name}}:
+		value = get{{.Name | ToKeyFuncBaseName}}JSON(data){{end}}
+	}
+	return
+}
+`
+
+// getter
+const tplGetter = `
+// Getter{{$fieldName := .FieldName}}{{range $index, $key := .Keys}}
+func get{{$key.Name | ToKeyFuncBaseName}}(data _ConnectionData) (value {{$key.Type | ToKeyTypeRealData}}) {
+	value, _ = getConnectionDataKey(data, {{$fieldName}}, {{$key.Name}}).({{$key.Type | ToKeyTypeRealData}})
+	return
+}{{end}}
+`
+
+// setter
+const tplSetter = `
+// Setter{{$fieldName := .FieldName}}{{range $index, $key := .Keys}}
+func set{{$key.Name | ToKeyFuncBaseName}}(data _ConnectionData, value {{$key.Type | ToKeyTypeRealData}}) {
+	setConnectionDataKey(data, {{$fieldName}}, {{$key.Name}}, value)
+}{{end}}
+`
+
+// json getter
+const tplJSONGetter = `
+// JSON Getter{{$fieldName := .FieldName}}{{range $index, $key := .Keys}}
+func get{{$key.Name | ToKeyFuncBaseName}}JSON(data _ConnectionData) (valueJSON string) {
+	valueJSON = getConnectionDataKeyJSON(data, {{$fieldName}}, {{$key.Name}}, get{{$fieldName | ToFieldFuncBaseName}}KeyType({{$key.Name}}))
+	return
+}{{end}}
+`
+
+// json setter
+const tplJSONSetter = `
+// JSON Setter{{$fieldName := .FieldName}}{{range $index, $key := .Keys}}
+func set{{$key.Name | ToKeyFuncBaseName}}JSON(data _ConnectionData, valueJSON string) {
+	setConnectionDataKeyJSON(data, {{$fieldName}}, {{$key.Name}}, valueJSON, get{{$fieldName | ToFieldFuncBaseName}}KeyType({{$key.Name}}))
+}{{end}}
+`
+
+// remover
+const tplRemover = `
+// Remover{{$fieldName := .FieldName}}{{range $index, $key := .Keys}}
+func remove{{$key.Name | ToKeyFuncBaseName}}(data _ConnectionData) {
+	removeConnectionDataKey(data, {{$fieldName}}, {{$key.Name}})
+}{{end}}
+`
+
+// TODO
+const tplGetAvaiableValues = `// Get avaiable values`
