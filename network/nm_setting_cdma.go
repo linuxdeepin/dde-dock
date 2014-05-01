b9d1dd6dbc0c5c03232acd53ca6d338a43348377
Xu FaSheng <fasheng.xu@gmail.com>
2014-05-01 23:28:21 +0800

    network: add ready code for field gsm and cdma

---
+++ network/nm_setting_cdma.go
@@ -0,0 +1,12 @@
+package main
+
+// TODO doc
+
+const NM_SETTING_CDMA_SETTING_NAME = "cdma"
+
+const (
+	NM_SETTING_CDMA_NUMBER         = "number"
+	NM_SETTING_CDMA_USERNAME       = "username"
+	NM_SETTING_CDMA_PASSWORD       = "password"
+	NM_SETTING_CDMA_PASSWORD_FLAGS = "password-flags"
+)
