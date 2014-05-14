50b8e17a80d5e6a1a52d51c956aa74e05f99eabc
snyh <snyh@snyh.org>
2014-05-14 08:50:40 +0800

    audio: destroy Meter when there hasn't tick event

--- audio/meter.go
+++ audio/meter.go
@@ -6,48 +6,58 @@ import "fmt"
 import "time"
 
 type Meter struct {
-	Volume float64
-	id     string
+	Volume  float64
+	id      string
+	hasTick bool
+	core    *pulse.SourceMeter
 }
 
+func (m *Meter) quit() {
+	delete(meters, m.id)
+	dbus.UnInstallObject(m)
+	m.core.Destroy()
+}
+
+func (m *Meter) tryQuit() {
+	defer m.quit()
+
+	for {
+		select {
+		case <-time.After(time.Second * 2):
+			if !m.hasTick {
+				return
+			}
+			m.hasTick = false
+		}
+	}
+}
 func (m *Meter) Tick() {
-	GetMeterManager().tick(m.id)
+	m.hasTick = true
 }
 
-type MeterManager map[string]*Meter
-
-var GetMeterManager = func() func() MeterManager {
-	m := MeterManager(make(map[string]*Meter))
-	return func() MeterManager {
-		return m
-	}
-}()
-
-func (mm MeterManager) clean() {
-	select {
-	case <-time.After(time.Second * 2):
-	}
-}
-func (MeterManager) tick(id string) {
-}
-func (mm MeterManager) getSourceMeter(s *Source) *Meter {
-	id := fmt.Sprintf("source%d", s.core.Index)
-	m, ok := mm[id]
-	if !ok {
-		m = &Meter{id: id}
-		dbus.InstallOnSession(m)
-		mm[id] = m
-		mm := pulse.NewSourceMeter(pulse.GetContext(), s.core.Index)
-		fmt.Println("CreateMeter...", id)
-		mm.ConnectChanged(func(v float64) {
-			m.setPropVolume(v)
-		})
-	}
+//TODO: use pulse.Meter instead of remove pulse.SourceMeter
+func NewMeter(id string, core *pulse.SourceMeter) *Meter {
+	m := &Meter{id: id, core: core}
+	m.Tick()
+	go m.tryQuit()
 	return m
 }
 
+var meters = make(map[string]*Meter)
+
 func (s *Source) GetMeter() *Meter {
-	return GetMeterManager().getSourceMeter(s)
+	id := fmt.Sprintf("source%d", s.core.Index)
+	m, ok := meters[id]
+	if !ok {
+		core := pulse.NewSourceMeter(pulse.GetContext(), s.core.Index)
+		core.ConnectChanged(func(v float64) {
+			m.setPropVolume(v)
+		})
+		m = NewMeter(id, core)
+		dbus.InstallOnSession(m)
+		meters[id] = m
+	}
+	return m
 }
 
 func (s *Sink) GetMeter() *Meter {
