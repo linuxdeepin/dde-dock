f5c93fc243f607d9fca18716808cd8030037969c
Xu FaSheng <fasheng.xu@gmail.com>
2014-05-29 17:25:12 +0800

    network: update test code

--- network/manager_test.go
+++ network/manager_test.go
@@ -9,10 +9,10 @@ func init() {
 	Suite(manager)
 }
 
-func (m *Manager) TestRemoveDevice(c *C) {
-	devs := make([]*device, 0)
-	devs = append(devs, &device{Path: "path1", State: 0})
-	devs = append(devs, &device{Path: "path2", State: 0})
-	devs = m.doRemoveDevice(devs, "path1")
-	c.Check(len(devs), Equals, 1)
-}
+// func (m *Manager) TestRemoveDevice(c *C) {
+// 	devs := make([]*device, 0)
+// 	devs = append(devs, &device{Path: "path1", State: 0})
+// 	devs = append(devs, &device{Path: "path2", State: 0})
+// 	devs = m.doRemoveDevice(devs, "path1")
+// 	c.Check(len(devs), Equals, 1)
+// }
