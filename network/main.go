358f9eaa637f315c7e091bd802110ce4cb5333d0
Xu FaSheng <fasheng.xu@gmail.com>
2014-05-29 17:07:34 +0800

    network: add start(), stop() in main.go

--- network/main.go
+++ network/main.go
@@ -4,17 +4,14 @@ import (
 	"dlib"
 	"dlib/dbus"
 	liblogger "dlib/logger"
-	"flag"
-	"os"
 )
 
 var (
-	logger   = liblogger.NewLogger(dbusNetworkDest)
-	manager  *Manager
-	argDebug bool
+	logger  = liblogger.NewLogger(dbusNetworkDest)
+	manager *Manager
 )
 
-func main() {
+func start() {
 	defer logger.EndTracing()
 
 	if !dlib.UniqueOnSession(dbusNetworkDest) {
@@ -22,27 +19,22 @@ func main() {
 		return
 	}
 
-	// configure logger
-	flag.BoolVar(&argDebug, "d", false, "debug mode")
-	flag.BoolVar(&argDebug, "debug", false, "debug mode")
-	flag.Parse()
-	if argDebug {
-		logger.SetLogLevel(liblogger.LEVEL_DEBUG)
-	}
-
 	manager = NewManager()
 	err := dbus.InstallOnSession(manager)
 	if err != nil {
 		logger.Error("register dbus interface failed: ", err)
-		os.Exit(1)
+		return
 	}
 
-	// initialize manager after configuring dbus
+	// initialize manager after dbus installed
 	manager.initManager()
 
 	dbus.DealWithUnhandledMessage()
 	if err := dbus.Wait(); err != nil {
 		logger.Error("lost dbus session:", err)
-		os.Exit(1)
+		return
 	}
+}
+
+func stop() {
 }
