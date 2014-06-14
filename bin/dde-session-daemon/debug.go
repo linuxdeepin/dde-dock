00bdac0cfcb138f1f0dad23d3a39b9ec38e55a3f
snyh <snyh@snyh.org>
2014-06-14 16:04:10 +0800

    bin/dde-session-daemon disable debug

--- bin/dde-session-daemon/debug.go
+++ bin/dde-session-daemon/debug.go
@@ -1,8 +1,8 @@
 package main
 
-import _ "net/http/pprof"
-import "net/http"
+//import _ "net/http/pprof"
+//import "net/http"
 
-func init() {
-	go http.ListenAndServe("localhost:6060", nil)
-}
+//func init() {
+//go http.ListenAndServe("localhost:6060", nil)
+//}
