a4a63db17af3623d9ebcb820be120523f5646ece
Xu FaSheng <fasheng.xu@gmail.com>
2014-06-14 10:31:19 +0800

    dde-session-daemon: disable bluetooth

--- bin/dde-session-daemon/daemon.go
+++ bin/dde-session-daemon/daemon.go
@@ -12,7 +12,8 @@ import _ "dde-daemon/datetime"
 import _ "dde-daemon/mime"
 
 import _ "dde-daemon/screen_edges"
-import _ "dde-daemon/bluetooth"
+
+// import _ "dde-daemon/bluetooth"
 
 import _ "dde-daemon/network"
 import _ "dde-daemon/mounts"
