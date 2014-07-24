cc2893437272fbf029df78e9a7214e987ebdb08a
liliqiang <kos1990l@gmail.com>
2014-07-24 14:09:39 +0800

    delete DDE_SESSION_PROCESS_COOKIE_ID from environment.

--- bin/dde-session-daemon/session_register.go
+++ bin/dde-session-daemon/session_register.go
@@ -2,9 +2,11 @@ package main
 
 import "dbus/com/deepin/sessionmanager"
 import "os"
+import "pkg.linuxdeepin.com/lib/utils"
 
 func ddeSessionRegister() {
 	cookie := os.ExpandEnv("$DDE_SESSION_PROCESS_COOKIE_ID")
+	utils.UnsetEnv("DDE_SESSION_PROCESS_COOKIE_ID")
 	manager, err := sessionmanager.NewSessionManager("com.deepin.SessionManager", "/com/deepin/SessionManager")
 	if err != nil {
 		return
