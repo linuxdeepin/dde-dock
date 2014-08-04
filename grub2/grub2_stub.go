16a936d83660394181399ecef344b640aaf2ab8b
Xu FaSheng <fasheng.xu@gmail.com>
2014-08-04 08:54:58 +0800

    grub2: refactor code, split dbus funtions to grub2_ifc.go

--- grub2/grub2_stub.go
+++ grub2/grub2_stub.go
@@ -22,27 +22,8 @@
 package grub2
 
 import (
-	"encoding/json"
-	"fmt"
 	"pkg.linuxdeepin.com/lib/dbus"
-	. "pkg.linuxdeepin.com/lib/gettext"
-	"strings"
 )
-
-const (
-	dbusGrubDest = "com.deepin.daemon.Grub2"
-	dbusGrubPath = "/com/deepin/daemon/Grub2"
-	dbusGrubIfs  = "com.deepin.daemon.Grub2"
-)
-
-// GetDBusInfo implements interface of dbus.DBusObject.
-func (grub *Grub2) GetDBusInfo() dbus.DBusInfo {
-	return dbus.DBusInfo{
-		dbusGrubDest,
-		dbusGrubPath,
-		dbusGrubIfs,
-	}
-}
 
 // OnPropertiesChanged implements interface of dbus.DBusObject.
 func (grub *Grub2) OnPropertiesChanged(name string, oldv interface{}) {
@@ -128,61 +109,4 @@ func (grub *Grub2) setPropResolution(value string) {
 func (grub *Grub2) setPropUpdating(value bool) {
 	grub.Updating = value
 	dbus.NotifyChange(grub, "Updating")
-}
-
-// GetSimpleEntryTitles return entry titles in level one.
-func (grub *Grub2) GetSimpleEntryTitles() ([]string, error) {
-	entryTitles := make([]string, 0)
-	for _, entry := range grub.entries {
-		if entry.parentSubMenu == nil && entry.entryType == MENUENTRY {
-			title := entry.getFullTitle()
-			if !strings.Contains(title, "memtest86+") {
-				entryTitles = append(entryTitles, title)
-			}
-		}
-	}
-	if len(entryTitles) == 0 {
-		err := fmt.Errorf("there is no menu entry in %s", grubMenuFile)
-		return entryTitles, err
-	}
-	return entryTitles, nil
-}
-
-func (grub *Grub2) GetAvailableResolutions() (modesJSON string, err error) {
-	type mode struct{ Text, Value string }
-	primaryResolution := getPrimaryScreenBestResolutionStr()
-	appendModeUniq := func(modes []mode, r string) []mode {
-		if r != primaryResolution {
-			modes = append(modes, mode{Text: r, Value: r})
-		}
-		return modes
-	}
-	var modes []mode
-	modes = append(modes, mode{Text: Tr("Auto"), Value: "auto"})
-	modes = append(modes, mode{Text: primaryResolution, Value: primaryResolution})
-	modes = appendModeUniq(modes, "1440x900")
-	modes = appendModeUniq(modes, "1400x1050")
-	modes = appendModeUniq(modes, "1280x800")
-	modes = appendModeUniq(modes, "1280x720")
-	modes = appendModeUniq(modes, "1366x768")
-	modes = appendModeUniq(modes, "1024x768")
-	modes = appendModeUniq(modes, "800x600")
-	tmpByteArray, err := json.Marshal(modes)
-	modesJSON = string(tmpByteArray)
-	return
-}
-
-// Reset reset all configuretion.
-func (grub *Grub2) Reset() {
-	simpleEntryTitles, _ := grub.GetSimpleEntryTitles()
-	firstEntry := ""
-	if len(simpleEntryTitles) > 0 {
-		firstEntry = simpleEntryTitles[0]
-	}
-	grub.setPropFixSettingsAlways(true)
-	grub.setPropEnableTheme(true)
-	grub.setPropResolution(getPrimaryScreenBestResolutionStr())
-	grub.setPropDefaultEntry(firstEntry)
-	grub.setPropTimeout(int32(10))
-	grub.theme.reset()
 }
