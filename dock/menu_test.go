5d0b1ca7a8f68e44f6c78e8983911cefd02ab988
liliqiang <liliqiang@linuxdeepin.com>
2014-10-17 09:34:09 +0800

    dock:: add test data.
    Change-Id: I84641e5ae24038f99a89fe432a565d8706e511c0

--- dock/menu_test.go
+++ dock/menu_test.go
@@ -11,7 +11,11 @@ var _ = C.Suite(&MenuTestSuite{})
 
 func (m *MenuTestSuite) TestGenerateMenuJson(c *C.C) {
 	initDeepin()
-	f := NewNormalApp("./test-data/firefox.desktop")
+	f := NewNormalApp("./testdata/firefox.desktop")
+	if f == nil {
+		c.Skip("get test data failed")
+		return
+	}
 	f.buildMenu()
 
 	c.Check(f.Menu, C.Equals, "{\"checkableMenu\":false, \"singleCheck\": false, \"items\":[{\"itemId\":\"1\", \"itemText\": \"_Run\", \"isActive\": true, \"isCheckable\":false, \"checked\":false, \"itemIcon\":\"\", \"itemIconHover\":\"\", \"itemIconInactive\":\"\", \"showCheckMark\":false, \"itemSubMenu\":{\"checkableMenu\":false, \"singleCheck\":false, \"items\": []}},{\"itemId\":\"2\", \"itemText\": \"新建窗口\", \"isActive\": true, \"isCheckable\":false, \"checked\":false, \"itemIcon\":\"\", \"itemIconHover\":\"\", \"itemIconInactive\":\"\", \"showCheckMark\":false, \"itemSubMenu\":{\"checkableMenu\":false, \"singleCheck\":false, \"items\": []}},{\"itemId\":\"3\", \"itemText\": \"新建隐私浏览窗口\", \"isActive\": true, \"isCheckable\":false, \"checked\":false, \"itemIcon\":\"\", \"itemIconHover\":\"\", \"itemIconInactive\":\"\", \"showCheckMark\":false, \"itemSubMenu\":{\"checkableMenu\":false, \"singleCheck\":false, \"items\": []}},{\"itemId\":\"4\", \"itemText\": \"_Undock\", \"isActive\": true, \"isCheckable\":false, \"checked\":false, \"itemIcon\":\"\", \"itemIconHover\":\"\", \"itemIconInactive\":\"\", \"showCheckMark\":false, \"itemSubMenu\":{\"checkableMenu\":false, \"singleCheck\":false, \"items\": []}}]}")
