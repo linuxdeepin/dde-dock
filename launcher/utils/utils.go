442de897b589db46a037a600a39aad47db087870
liliqiang <liliqiang@linuxdeepin.com>
2014-12-17 15:52:46 +0800

    launcher:: fix sort problem.
    gets wrong time installed.
    
    tower:https://tower.im/projects/8f19f0bf0e754f0b82ef2c24bc230973/todos/75091516c5a34c4c9e43737431f00c49/
    Change-Id: I1a88b62403c22c2ee023b42ed6514d896b57dcc7

--- launcher/utils/utils.go
+++ launcher/utils/utils.go
@@ -2,8 +2,18 @@ package utils
 
 import (
 	"os"
+	"pkg.linuxdeepin.com/lib/gio-2.0"
+	"strings"
 )
 
 const (
 	DirDefaultPerm os.FileMode = 0755
 )
+
+func CreateDesktopAppInfo(name string) *gio.DesktopAppInfo {
+	if strings.ContainsRune(name, os.PathSeparator) {
+		return gio.NewDesktopAppInfoFromFilename(name)
+	} else {
+		return gio.NewDesktopAppInfo(name)
+	}
+}
