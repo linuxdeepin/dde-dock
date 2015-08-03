ea069679ceaf9aeefbe825066a6b0c497dbe9b4d
Hualet Wang <mr.asianwang@gmail.com>
2015-08-03 15:37:57 +0800

    update of AppIcon setIcon method
    take care of the case that the iconPath is an absolute path of
    an image or the case that the iconPath is a string representing
    an inline image.

--- dde-dock/src/Widgets/appicon.cpp
+++ dde-dock/src/Widgets/appicon.cpp
@@ -1,3 +1,5 @@
+#include <QFile>
+
 #include "appicon.h"
 #include "Controller/themeiconmanager.h"
 
@@ -13,11 +15,25 @@ void AppIcon::setIcon(const QString &iconPath)
     ThemeIconManager * themeIconManager = ThemeIconManager::instance();
 
     QIcon icon = themeIconManager->getIcon(iconPath);
+    QPixmap pixmap;
 
     if (icon.isNull()) {
-        qDebug() << iconPath;
+        // iconPath is an absolute path of the system.
+        if (QFile::exists(iconPath)) {
+            pixmap = QPixmap(iconPath);
+        } else if (iconPath.startsWith("data:image/")){
+            // iconPath is a string representing an inline image.
+            QStringList strs = iconPath.split("base64,");
+            if (strs.length() == 2) {
+                QByteArray data = QByteArray::fromBase64(strs.at(1).toLatin1());
+                pixmap.loadFromData(data);
+            }
+        }
     } else {
-        QPixmap pixmap = icon.pixmap(48, 48);
+        pixmap = icon.pixmap(48, 48);
+    }
+
+    if (!pixmap.isNull()) {
         pixmap = pixmap.scaled(m_modeData->getAppIconSize(),
                                m_modeData->getAppIconSize(),
                                Qt::KeepAspectRatioByExpanding,
