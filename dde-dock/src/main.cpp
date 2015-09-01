7fc70a5d97853b33ec8f3be56f5df55a5c20887f
Hualet Wang <mr.asianwang@gmail.com>
2015-09-01 11:23:14 +0800

    register dde session at application start point
    Change-Id: I31d034782666be1e96aade75f325ca0fe796063c

--- dde-dock/src/main.cpp
+++ dde-dock/src/main.cpp
@@ -1,7 +1,26 @@
 #include <QApplication>
 #include <QFile>
 #include <QDebug>
+#include <QDBusConnection>
+
 #include "mainwidget.h"
+
+// let startdde know that we've already started.
+void RegisterDdeSession()
+{
+    char *envName = "DDE_SESSION_PROCESS_COOKIE_ID";
+
+    QByteArray cookie = qgetenv(envName);
+    qunsetenv(envName);
+
+    if (!cookie.isEmpty()) {
+        QDBusInterface iface("com.deepin.SessionManager",
+                             "/com/deepin/SessionManager",
+                             "com.deepin.SessionManager",
+                             QDBusConnection::sessionBus());
+        iface.asyncCall("Register", QString(cookie));
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -10,25 +29,24 @@ int main(int argc, char *argv[])
     a.setApplicationName("dde-dock");
     a.setApplicationDisplayName("Dock");
 
-    if(QDBusConnection::sessionBus().registerService(DBUS_NAME)){
+    if (QDBusConnection::sessionBus().registerService(DBUS_NAME)) {
         QFile file("://Resources/qss/default.qss");
-        if (file.open(QFile::ReadOnly))
-        {
+        if (file.open(QFile::ReadOnly)) {
             QString styleSheet = QLatin1String(file.readAll());
             qApp->setStyleSheet(styleSheet);
             file.close();
-        }
-        else
-        {
+        } else {
             qWarning() << "[Error:] Open  style file errr!";
         }
+
 
         MainWidget w;
         w.show();
 
+        RegisterDdeSession();
+
         return a.exec();
-    }
-    else {
+    } else {
         qWarning() << "dde dock is running...";
         return 0;
     }
