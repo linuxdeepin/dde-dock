01e84c5a217fa1fe6348d876c1be0217be83d055
Hualet Wang <mr.asianwang@gmail.com>
2015-09-28 11:18:42 +0800

    setup tx and ts files
    Change-Id: I7e347449b73fe45157063a6f7bf6da0146355c87

--- dde-dock-bundle.pro
+++ dde-dock-bundle.pro
@@ -2,3 +2,6 @@ TEMPLATE = subdirs
 SUBDIRS = dde-dock \
           dde-dock-systray-plugin \
           dde-dock-trash-plugin
+
+TRANSLATIONS += translations/dde-dock.ts \
+                translations/dde-dock_zh_CN.ts
---
+++ translations/dde-dock_zh_CN.ts
@@ -0,0 +1,117 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ClearTrashDialog</name>
+    <message>
+        <location filename="../dde-dock-trash-plugin/dialogs/cleartrashdialog.cpp" line="12"/>
+        <source>Are you sure to clear items in trash?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock-trash-plugin/dialogs/cleartrashdialog.cpp" line="13"/>
+        <source>This action cannot be restored</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock-trash-plugin/dialogs/cleartrashdialog.cpp" line="15"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock-trash-plugin/dialogs/cleartrashdialog.cpp" line="15"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmUninstallDialog</name>
+    <message>
+        <location filename="../dde-dock-trash-plugin/dialogs/confirmuninstalldialog.cpp" line="10"/>
+        <source>Are you sure to uninstall this application?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock-trash-plugin/dialogs/confirmuninstalldialog.cpp" line="11"/>
+        <source>This action cannot be restored</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock-trash-plugin/dialogs/confirmuninstalldialog.cpp" line="13"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock-trash-plugin/dialogs/confirmuninstalldialog.cpp" line="13"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainItem</name>
+    <message>
+        <location filename="../dde-dock-trash-plugin/mainitem.cpp" line="96"/>
+        <source>Are you sure to uninstall &quot;%1&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PanelMenu</name>
+    <message>
+        <location filename="../dde-dock/src/panel/panelmenu.cpp" line="35"/>
+        <source>Fashion mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock/src/panel/panelmenu.cpp" line="36"/>
+        <source>Efficient mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock/src/panel/panelmenu.cpp" line="37"/>
+        <source>Classic mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock/src/panel/panelmenu.cpp" line="39"/>
+        <source>Keep showing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock/src/panel/panelmenu.cpp" line="40"/>
+        <source>Keep hidden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock/src/panel/panelmenu.cpp" line="41"/>
+        <source>Smart hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock/src/panel/panelmenu.cpp" line="43"/>
+        <source>Notification area settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PluginsSettingFrame</name>
+    <message>
+        <location filename="../dde-dock/src/controller/plugins/pluginssettingframe.cpp" line="149"/>
+        <source>Notification Area Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrashPlugin</name>
+    <message>
+        <location filename="../dde-dock-trash-plugin/trashplugin.cpp" line="22"/>
+        <source>Trash</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-dock-trash-plugin/trashplugin.cpp" line="82"/>
+        <source>Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
