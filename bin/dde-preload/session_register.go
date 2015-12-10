5a639857bedf8543fa805d21a24380c6fd89af89
liliqiang <liliqiang@linuxdeepin.com>
2015-12-10 17:30:16 +0800

    preload: fix start timeout from startdde.
    Change-Id: Id870fd8ac6836cc1feeffbee437d1cdf81d34cd5

--- bin/dde-preload/session_register.go
+++ bin/dde-preload/session_register.go
@@ -8,12 +8,15 @@ func ddeSessionRegister() {
 	cookie := os.ExpandEnv("$DDE_SESSION_PROCESS_COOKIE_ID")
 	utils.UnsetEnv("DDE_SESSION_PROCESS_COOKIE_ID")
 	if cookie == "" {
+		logger.Warning("get DDE_SESSION_PROCESS_COOKIE_ID failed")
 		return
 	}
-	manager, err := sessionmanager.NewSessionManager("com.deepin.SessionManager", "/com/deepin/SessionManager")
-	if err != nil {
-		logger.Warning("register failed:", err)
-		return
-	}
-	manager.Register(cookie)
+	go func() {
+		manager, err := sessionmanager.NewSessionManager("com.deepin.SessionManager", "/com/deepin/SessionManager")
+		if err != nil {
+			logger.Warning("register failed:", err)
+			return
+		}
+		manager.Register(cookie)
+	}()
 }
