e2341614cababa95c7fb6709fa80f339a32df889
electricface <songwentai@linuxdeepin.com>
2016-03-30 15:50:32 +0800

    dock: Clean up redundant code in settings.go
    Change-Id: I337b7082aa45a9476f581539351e44825184fb2a

--- dock/settings.go
+++ dock/settings.go
@@ -22,9 +22,6 @@ import (
 const (
 	HideModeKey    string = "hide-mode"
 	DisplayModeKey string = "display-mode"
-	ClockTypeKey   string = "clock-type"
-	DisplayDateKey string = "display-date"
-	DisplayWeekKey string = "display-week"
 )
 
 type HideModeType int32
@@ -72,24 +69,6 @@ func (t DisplayModeType) String() string {
 	}
 }
 
-type ClockType int32
-
-const (
-	ClockTypeDigit ClockType = iota
-	ClockTypeAnalog
-)
-
-func (self ClockType) String() string {
-	switch self {
-	case ClockTypeDigit:
-		return "digit clock"
-	case ClockTypeAnalog:
-		return "analog clock"
-	default:
-		return "unknown type clock"
-	}
-}
-
 // Setting存储dock相关的设置。
 type Setting struct {
 	core *gio.Settings
@@ -100,25 +79,10 @@ type Setting struct {
 	displayModeLock sync.RWMutex
 	displayMode     DisplayModeType
 
-	clockTypeLock sync.RWMutex
-	clockType     ClockType
-
-	displayDateLock sync.RWMutex
-	displayDate     bool
-
-	displayWeekLock sync.RWMutex
-	displayWeek     bool
-
 	// HideModeChanged在dock的隐藏模式改变后触发，返回改变后的模式。
 	HideModeChanged func(mode int32)
 	// DisplayModeChanged在dock的显示模式改变后触发，返回改变后的模式。
 	DisplayModeChanged func(mode int32)
-	// ClockTypeChanged在dock的时钟模式改变后触发，返回改变后的模式。
-	ClockTypeChanged func(mode int32)
-	// DisplayDateChanged在dock的显示日期设置改变后触发，返回是否显示日期。
-	DisplayDateChanged func(bool)
-	// DisplayWeekChanged在dock的显示星期设置改变后触发，返回是否显示星期。
-	DisplayWeekChanged func(bool)
 }
 
 func NewSetting() *Setting {
@@ -146,17 +110,6 @@ func (s *Setting) init() bool {
 		s.doSetDisplayMode(value)
 	})
 
-	s.listenSettingChange(ClockTypeKey, func(*gio.Settings, string) {
-		s.doSetClockType(ClockType(s.core.GetEnum(ClockTypeKey)))
-	})
-
-	s.listenSettingChange(DisplayDateKey, func(*gio.Settings, string) {
-		s.doSetDisplayDate(s.core.GetBoolean(DisplayDateKey))
-	})
-	s.listenSettingChange(DisplayWeekKey, func(*gio.Settings, string) {
-		s.doSetDisplayWeek(s.core.GetBoolean(DisplayWeekKey))
-	})
-
 	// at least one read operation must be called after signal connected, otherwise,
 	// the signal connection won't work from glib 2.43.
 	// NB: https://github.com/GNOME/glib/commit/8ff5668a458344da22d30491e3ce726d861b3619
@@ -166,10 +119,6 @@ func (s *Setting) init() bool {
 		s.hideMode = HideModeSmartHide
 		s.core.SetEnum(HideModeKey, int32(HideModeSmartHide))
 	}
-	s.clockType = ClockType(s.core.GetEnum(ClockTypeKey))
-	s.displayDate = s.core.GetBoolean(DisplayDateKey)
-	s.displayWeek = s.core.GetBoolean(DisplayWeekKey)
-
 	return true
 }
 
@@ -247,39 +196,6 @@ func (s *Setting) doSetHideMode(value HideModeType) {
 	dbus.Emit(s, "HideModeChanged", int32(value))
 }
 
-func (s *Setting) doSetClockType(value ClockType) {
-	s.clockTypeLock.Lock()
-	defer s.clockTypeLock.Unlock()
-	if value == s.clockType {
-		return
-	}
-	s.clockType = value
-	dbus.Emit(s, "ClockTypeChanged", int32(s.clockType))
-
-}
-
-func (s *Setting) doSetDisplayDate(value bool) {
-	s.displayDateLock.Lock()
-	defer s.displayDateLock.Unlock()
-	if value == s.displayDate {
-		return
-	}
-	s.displayDate = value
-	dbus.Emit(s, "DisplayDateChanged", s.displayDate)
-}
-
-func (s *Setting) doSetDisplayWeek(value bool) {
-	s.displayWeekLock.Lock()
-	defer s.displayWeekLock.Unlock()
-
-	if s.displayWeek == value {
-		return
-	}
-
-	s.displayWeek = value
-	dbus.Emit(s, "DisplayWeekChanged", s.displayWeek)
-}
-
 func (s *Setting) listenSettingChange(key string, handler func(*gio.Settings, string)) {
 	signalDetial := fmt.Sprintf("changed::%s", key)
 	logger.Debugf("connect to %s signal", signalDetial)
@@ -311,44 +227,6 @@ func (s *Setting) SetDisplayMode(_mode int32) bool {
 	logger.Debug("[Setting.SetDisplayMode]:", mode)
 	ok := s.core.SetEnum(DisplayModeKey, int32(mode))
 	s.doSetDisplayMode(mode)
-	return ok
-}
-
-// GetClockType获取dock当前的始终模式。
-func (s *Setting) GetClockType() int32 {
-	return int32(s.clockType)
-}
-
-// SetClockType设置dock的时钟显示模式。
-func (s *Setting) SetClockType(_clockType int32) bool {
-	clockType := ClockType(_clockType)
-	logger.Debug("clock type changed to:", clockType)
-	ok := s.core.SetEnum(ClockTypeKey, int32(clockType))
-	s.doSetClockType(clockType)
-	return ok
-}
-
-// GetDisplayDate获取是否显示日期。
-func (s *Setting) GetDisplayDate() bool {
-	return s.displayDate
-}
-
-// SetDisplayDate设置是否显示日期。
-func (s *Setting) SetDisplayDate(d bool) bool {
-	ok := s.core.SetBoolean(DisplayDateKey, d)
-	s.doSetDisplayDate(d)
-	return ok
-}
-
-// GetDisplayWeek获取是否显示星期。
-func (s *Setting) GetDisplayWeek() bool {
-	return s.displayWeek
-}
-
-// SetDisplayWeek设置是否显示星期。
-func (s *Setting) SetDisplayWeek(d bool) bool {
-	ok := s.core.SetBoolean(DisplayWeekKey, d)
-	s.doSetDisplayWeek(d)
 	return ok
 }
 
