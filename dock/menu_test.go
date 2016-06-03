77f763fe29f2bd3a68df11aa84613f8c10a9cb45
electricface <songwentai@linuxdeepin.com>
2016-06-03 09:46:13 +0800

    dock: Add test for method GenerateMenuJson
    Change-Id: Ia1013fc12ac3ba85212823ff8a23aa61c8a98daf

--- dock/menu_test.go
+++ dock/menu_test.go
@@ -7,30 +7,27 @@
  * (at your option) any later version.
  **/
 
-// Package main provides ...
 package dock
 
-// import (
-// 	C "launchpad.net/gocheck"
-// 	"os"
-// )
-//
-// type MenuTestSuite struct{}
-//
-// var _ = C.Suite(&MenuTestSuite{})
-//
-// func (m *MenuTestSuite) TestGenerateMenuJson(c *C.C) {
-// 	old := os.Getenv("LANGUAGE")
-// 	os.Setenv("LANGUAGE", "zh_CN")
-// 	f := NewNormalApp("./testdata/firefox.desktop")
-// 	if f == nil {
-// 		c.Skip("get test data failed")
-// 		return
-// 	}
-// 	core := NewAppInfoFromFile("./testdata/firefox.desktop")
-// 	f.buildMenu(core)
-// 	core.Destroy()
-// 	os.Setenv("LANGUAGE", old)
-//
-// 	c.Check(f.Menu, C.Equals, "{\"checkableMenu\":false, \"singleCheck\": false, \"items\":[{\"itemId\":\"1\", \"itemText\": \"_Run\", \"isActive\": true, \"isCheckable\":false, \"checked\":false, \"itemIcon\":\"\", \"itemIconHover\":\"\", \"itemIconInactive\":\"\", \"showCheckMark\":false, \"itemSubMenu\":{\"checkableMenu\":false, \"singleCheck\":false, \"items\": []}},{\"itemId\":\"2\", \"itemText\": \"新建窗口\", \"isActive\": true, \"isCheckable\":false, \"checked\":false, \"itemIcon\":\"\", \"itemIconHover\":\"\", \"itemIconInactive\":\"\", \"showCheckMark\":false, \"itemSubMenu\":{\"checkableMenu\":false, \"singleCheck\":false, \"items\": []}},{\"itemId\":\"3\", \"itemText\": \"新建隐私浏览窗口\", \"isActive\": true, \"isCheckable\":false, \"checked\":false, \"itemIcon\":\"\", \"itemIconHover\":\"\", \"itemIconInactive\":\"\", \"showCheckMark\":false, \"itemSubMenu\":{\"checkableMenu\":false, \"singleCheck\":false, \"items\": []}},{\"itemId\":\"4\", \"itemText\": \"_Undock\", \"isActive\": true, \"isCheckable\":false, \"checked\":false, \"itemIcon\":\"\", \"itemIconHover\":\"\", \"itemIconInactive\":\"\", \"showCheckMark\":false, \"itemSubMenu\":{\"checkableMenu\":false, \"singleCheck\":false, \"items\": []}}]}")
-// }
+import (
+	"encoding/json"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func Test_GenerateMenuJson(t *testing.T) {
+	Convey("GenerateMenuJson", t, func() {
+		menu := NewMenu()
+		item0 := NewMenuItem("item 0", nil, true)
+		item1 := NewMenuItem("item 1", nil, true)
+		item2 := NewMenuItem("item 2", nil, true)
+		menu.AppendItem(item0, item1, item2)
+
+		menuJSON := menu.GenerateJSON()
+		So(menuJSON, ShouldEqual, `{"checkableMenu":false, "singleCheck": false, "items":[{"itemId":"1", "itemText": "item 0", "isActive": true, "isCheckable":false, "checked":false, "itemIcon":"", "itemIconHover":"", "itemIconInactive":"", "showCheckMark":false, "itemSubMenu":{"checkableMenu":false, "singleCheck":false, "items": []}},{"itemId":"2", "itemText": "item 1", "isActive": true, "isCheckable":false, "checked":false, "itemIcon":"", "itemIconHover":"", "itemIconInactive":"", "showCheckMark":false, "itemSubMenu":{"checkableMenu":false, "singleCheck":false, "items": []}},{"itemId":"3", "itemText": "item 2", "isActive": true, "isCheckable":false, "checked":false, "itemIcon":"", "itemIconHover":"", "itemIconInactive":"", "showCheckMark":false, "itemSubMenu":{"checkableMenu":false, "singleCheck":false, "items": []}}]}`)
+
+		var parseResult interface{}
+		err := json.Unmarshal([]byte(menuJSON), &parseResult)
+		So(err, ShouldBeNil)
+	})
+}
