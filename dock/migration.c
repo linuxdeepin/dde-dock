0242eed9bc623372d69ae81913be735f8e82dd14
electricface <songwentai@deepin.com>
2016-07-13 17:20:00 +0800

    dock: Replace get_theme_icon with getIconFilePath
    remove unused C code
    
    Change-Id: I7803597cd5c23f95bf5be1445301b8255a62cfa3

--- dock/migration.c
+++ dock/migration.c
@@ -7,122 +7,25 @@
  * (at your option) any later version.
  **/
 
-
-#include <glib.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <gio/gdesktopappinfo.h>
-#include <glib/gprintf.h>
-#include <sys/stat.h>
-#include <stdio.h>
 #include <gtk/gtk.h>
-#include <fcntl.h>
 
-char* get_basename_without_extend_name(char const* path);
-gboolean is_deepin_icon(char const* icon_path);
-char* check_absolute_path_icon(char const* app_id, char const* icon_path);
-gboolean is_chrome_app(char const* name);
-
-#define DATA_DIR "/usr/share/dde/data"
-#define DEEPIN_ICONS_PATH DATA_DIR"/deepin_icons.ini"
-
-static GKeyFile* deepin_icons = NULL;
-
-char* get_basename_without_extend_name(char const* path)
+char *get_icon_file_path(const char*name)
 {
-    g_assert(path!= NULL);
-    char* basename = g_path_get_basename(path);
-    char* ext_sep = strrchr(basename, '.');
-    if (ext_sep != NULL) {
-        char* basename_without_ext = g_strndup(basename, ext_sep - basename);
-        g_free(basename);
-        return basename_without_ext;
-    }
-
-    return basename;
-}
-
-gboolean is_deepin_icon(char const* icon_path)
-{
-    return g_str_has_prefix(icon_path, "/usr/share/icons/Deepin/");
-}
-
-
-char* icon_name_to_path(const char* name, int size)
-{
-    if (g_path_is_absolute(name))
-        return g_strdup(name);
-    g_return_val_if_fail(name != NULL, NULL);
-
-    int pic_name_len = strlen(name);
-    char* ext = strrchr(name, '.');
-    if (ext != NULL) {
-        if (g_ascii_strcasecmp(ext+1, "png") == 0
-            || g_ascii_strcasecmp(ext+1, "svg") == 0
-            || g_ascii_strcasecmp(ext+1, "jpg") == 0) {
-            pic_name_len = ext - name;
-            g_debug("desktop's Icon name should an absoulte path or an basename without extension");
-        }
-    }
-
-    GtkIconTheme* them = gtk_icon_theme_get_default(); //do not ref or unref it
-    if (them == NULL) {
-        // NOTE: the g_message won't be recorded on log,
-        g_message("error get default icon theme failed");
+    GtkIconTheme* theme = gtk_icon_theme_get_default();
+    if (theme == NULL) {
+        g_warning("error get default icon theme failed");
         return NULL;
     }
-
-    char* pic_name = g_strndup(name, pic_name_len);
-    GtkIconInfo* info = gtk_icon_theme_lookup_icon(them, pic_name, size, GTK_ICON_LOOKUP_GENERIC_FALLBACK);
-    g_free(pic_name);
+    const int size = 48;
+    GtkIconInfo* info = gtk_icon_theme_lookup_icon(theme, name, size, GTK_ICON_LOOKUP_GENERIC_FALLBACK);
     if (info) {
         char* path = g_strdup(gtk_icon_info_get_filename(info));
-#if GTK_MAJOR_VERSION >= 3
         g_object_unref(info);
-#elif GTK_MAJOR_VERSION == 2
-        gtk_icon_info_free(info);
-#endif
         return path;
-    } else {
-        return NULL;
     }
+    return NULL;
 }
-
-
-static char* _check(char const* app_id)
-{
-    char* icon = NULL;
-    char* temp_icon_name_holder = icon_name_to_path(app_id, 48);
-
-    if (temp_icon_name_holder != NULL) {
-        if (!g_str_has_prefix(temp_icon_name_holder, "data:image"))
-            icon = temp_icon_name_holder;
-        else
-            g_free(temp_icon_name_holder);
-    }
-
-    return icon;
-}
-
-
-char* check_absolute_path_icon(char const* app_id, char const* icon_path)
-{
-    char* icon = NULL;
-    if ((icon = _check(app_id)) == NULL) {
-        char* basename = get_basename_without_extend_name(icon_path);
-        if (basename != NULL) {
-            if (g_strcmp0(app_id, basename) == 0
-                || (icon = _check(basename)) == NULL) {
-                icon = g_strdup(icon_path);
-            }
-            g_free(basename);
-        }
-    }
-
-    return icon;
-}
-
 
 static char DE_NAME[100] = "DEEPIN";
 
