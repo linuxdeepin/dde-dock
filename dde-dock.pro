63c1c28897bdefca65e74b71f4535ad2b20156b4
石博文 <sbw@sbw.so>
2016-08-05 10:00:14 +0800

    add translations file
    Change-Id: I3225a2f9f2cf137cff8ac70c622000d3557a8e3f

--- dde-dock.pro
+++ dde-dock.pro
@@ -2,3 +2,15 @@
 TEMPLATE = subdirs
 SUBDIRS = frame \
           plugins
+
+
+# Automating generation .qm files from .ts files
+CONFIG(release, debug|release) {
+    system($$PWD/translate_generation.sh)
+}
+TRANSLATIONS    = translations/dde-dock.ts
+
+qm_files.path = $${PREFIX}/share/dde-dock/translations/
+qm_files.files = translations/*.qm
+
+INSTALLS = qm_files
---
+++ translations/dde-dock.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DatetimePlugin</name>
+    <message>
+        <location filename="../plugins/datetime/datetimeplugin.cpp" line="73"/>
+        <source>MM/dd/yyyy ddd HH:mm:ss</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DatetimeWidget</name>
+    <message>
+        <location filename="../plugins/datetime/datetimewidget.cpp" line="34"/>
+        <source>HH:mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DockSettings</name>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="18"/>
+        <source>Fashion Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="19"/>
+        <source>Efficient Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="20"/>
+        <source>Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="21"/>
+        <source>Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="22"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="23"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="24"/>
+        <source>Large</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="25"/>
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="26"/>
+        <source>Small</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="27"/>
+        <source>Keep Shown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="28"/>
+        <source>Keep Hidden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="29"/>
+        <source>Smart Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="64"/>
+        <source>Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="72"/>
+        <source>Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="79"/>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="86"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LauncherItem</name>
+    <message>
+        <location filename="../frame/item/launcheritem.cpp" line="17"/>
+        <source>Launcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PopupControlWidget</name>
+    <message>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="15"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="16"/>
+        <source>Clear</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SoundApplet</name>
+    <message>
+        <location filename="../plugins/sound/soundapplet.cpp" line="28"/>
+        <source>Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/sound/soundapplet.cpp" line="45"/>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WiredItem</name>
+    <message>
+        <location filename="../plugins/network/item/wireditem.cpp" line="35"/>
+        <source>Unknow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/wireditem.cpp" line="40"/>
+        <source>Disconnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WirelessApplet</name>
+    <message>
+        <location filename="../plugins/network/item/applet/wirelessapplet.cpp" line="288"/>
+        <source>Auto-connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/applet/wirelessapplet.cpp" line="294"/>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/applet/wirelessapplet.cpp" line="309"/>
+        <source>Please enter the password of &lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
