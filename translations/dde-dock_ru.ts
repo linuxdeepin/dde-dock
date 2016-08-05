713f9fa7f7f944de05dd87dc615eeede46efa796
transifex <transifex@linuxdeepin.com>
2016-08-05 06:01:04 +0000

    auto sync po files from transifex
    Change-Id: If34a3ae216a894b17090aa72cd571848f3c4f531

---
+++ translations/dde-dock_ru.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru" version="2.1">
+<context>
+    <name>DatetimePlugin</name>
+    <message>
+        <location filename="../plugins/datetime/datetimeplugin.cpp" line="73"/>
+        <source>MM/dd/yyyy ddd HH:mm:ss</source>
+        <translation>ММ/дд/гггг ддд ЧЧ:мм:сс</translation>
+    </message>
+</context>
+<context>
+    <name>DatetimeWidget</name>
+    <message>
+        <location filename="../plugins/datetime/datetimewidget.cpp" line="34"/>
+        <source>HH:mm</source>
+        <translation>ЧЧ:мм</translation>
+    </message>
+</context>
+<context>
+    <name>DockSettings</name>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="18"/>
+        <source>Fashion Mode</source>
+        <translation>Современный режим</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="19"/>
+        <source>Efficient Mode</source>
+        <translation>Эффектный режим</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="20"/>
+        <source>Top</source>
+        <translation>Сверху</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="21"/>
+        <source>Bottom</source>
+        <translation>Снизу</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="22"/>
+        <source>Left</source>
+        <translation>Слева</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="23"/>
+        <source>Right</source>
+        <translation>Справа</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="24"/>
+        <source>Large</source>
+        <translation>Большой</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="25"/>
+        <source>Medium</source>
+        <translation>Нормально</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="26"/>
+        <source>Small</source>
+        <translation>Маленький</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="27"/>
+        <source>Keep Shown</source>
+        <translation>Показывать</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="28"/>
+        <source>Keep Hidden</source>
+        <translation>Скрывать</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="29"/>
+        <source>Smart Hide</source>
+        <translation>Скрывать разумно</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="64"/>
+        <source>Mode</source>
+        <translation>Режим</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="72"/>
+        <source>Location</source>
+        <translation>Местонахождение</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="79"/>
+        <source>Size</source>
+        <translation>Размер</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="86"/>
+        <source>Status</source>
+        <translation>Положение</translation>
+    </message>
+</context>
+<context>
+    <name>LauncherItem</name>
+    <message>
+        <location filename="../frame/item/launcheritem.cpp" line="17"/>
+        <source>Launcher</source>
+        <translation> Лаунчер</translation>
+    </message>
+</context>
+<context>
+    <name>PopupControlWidget</name>
+    <message>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="15"/>
+        <source>Run</source>
+        <translation>Выполнить</translation>
+    </message>
+    <message>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="16"/>
+        <source>Empty</source>
+        <translation>Очистить</translation>
+    </message>
+</context>
+<context>
+    <name>SoundApplet</name>
+    <message>
+        <location filename="../plugins/sound/soundapplet.cpp" line="28"/>
+        <source>Device</source>
+        <translation>Устройство</translation>
+    </message>
+    <message>
+        <location filename="../plugins/sound/soundapplet.cpp" line="45"/>
+        <source>Application</source>
+        <translation>Приложения</translation>
+    </message>
+</context>
+<context>
+    <name>WiredItem</name>
+    <message>
+        <location filename="../plugins/network/item/wireditem.cpp" line="35"/>
+        <source>Unknow</source>
+        <translation>Неизвестное</translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/wireditem.cpp" line="40"/>
+        <source>Disconnect</source>
+        <translation>Отключить</translation>
+    </message>
+</context>
+<context>
+    <name>WirelessApplet</name>
+    <message>
+        <location filename="../plugins/network/item/applet/wirelessapplet.cpp" line="288"/>
+        <source>Auto-connect</source>
+        <translation>Авто-подключение</translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/applet/wirelessapplet.cpp" line="294"/>
+        <source>Connect</source>
+        <translation>Подключение</translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/applet/wirelessapplet.cpp" line="309"/>
+        <source>Password required to connect to &lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
---
+++ translations/dde-dock_zh_CN.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>DatetimePlugin</name>
+    <message>
+        <location filename="../plugins/datetime/datetimeplugin.cpp" line="73"/>
+        <source>MM/dd/yyyy ddd HH:mm:ss</source>
+        <translation>MM/dd/yyyy ddd HH:mm:ss</translation>
+    </message>
+</context>
+<context>
+    <name>DatetimeWidget</name>
+    <message>
+        <location filename="../plugins/datetime/datetimewidget.cpp" line="34"/>
+        <source>HH:mm</source>
+        <translation>HH:mm</translation>
+    </message>
+</context>
+<context>
+    <name>DockSettings</name>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="18"/>
+        <source>Fashion Mode</source>
+        <translation>时尚模式</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="19"/>
+        <source>Efficient Mode</source>
+        <translation>高效模式</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="20"/>
+        <source>Top</source>
+        <translation>上</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="21"/>
+        <source>Bottom</source>
+        <translation>下</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="22"/>
+        <source>Left</source>
+        <translation>左</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="23"/>
+        <source>Right</source>
+        <translation>右</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="24"/>
+        <source>Large</source>
+        <translation>大</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="25"/>
+        <source>Medium</source>
+        <translation>中</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="26"/>
+        <source>Small</source>
+        <translation>小</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="27"/>
+        <source>Keep Shown</source>
+        <translation>一直显示</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="28"/>
+        <source>Keep Hidden</source>
+        <translation>一直隐藏</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="29"/>
+        <source>Smart Hide</source>
+        <translation>智能隐藏</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="64"/>
+        <source>Mode</source>
+        <translation>模式</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="72"/>
+        <source>Location</source>
+        <translation>位置</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="79"/>
+        <source>Size</source>
+        <translation>大小</translation>
+    </message>
+    <message>
+        <location filename="../frame/util/docksettings.cpp" line="86"/>
+        <source>Status</source>
+        <translation>状态</translation>
+    </message>
+</context>
+<context>
+    <name>LauncherItem</name>
+    <message>
+        <location filename="../frame/item/launcheritem.cpp" line="17"/>
+        <source>Launcher</source>
+        <translation>启动器</translation>
+    </message>
+</context>
+<context>
+    <name>PopupControlWidget</name>
+    <message>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="15"/>
+        <source>Run</source>
+        <translation>运行</translation>
+    </message>
+    <message>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="16"/>
+        <source>Empty</source>
+        <translation>清空</translation>
+    </message>
+</context>
+<context>
+    <name>SoundApplet</name>
+    <message>
+        <location filename="../plugins/sound/soundapplet.cpp" line="28"/>
+        <source>Device</source>
+        <translation>设备</translation>
+    </message>
+    <message>
+        <location filename="../plugins/sound/soundapplet.cpp" line="45"/>
+        <source>Application</source>
+        <translation>应用</translation>
+    </message>
+</context>
+<context>
+    <name>WiredItem</name>
+    <message>
+        <location filename="../plugins/network/item/wireditem.cpp" line="35"/>
+        <source>Unknow</source>
+        <translation>未知</translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/wireditem.cpp" line="40"/>
+        <source>Disconnect</source>
+        <translation>断开</translation>
+    </message>
+</context>
+<context>
+    <name>WirelessApplet</name>
+    <message>
+        <location filename="../plugins/network/item/applet/wirelessapplet.cpp" line="288"/>
+        <source>Auto-connect</source>
+        <translation>自动连接</translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/applet/wirelessapplet.cpp" line="294"/>
+        <source>Connect</source>
+        <translation>连接</translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/applet/wirelessapplet.cpp" line="309"/>
+        <source>Password required to connect to &lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;</source>
+        <translation>连接&lt;font color=&quot;#faca57&quot;&gt;%1&lt;/font&gt;需要密码</translation>
+    </message>
+</context>
+</TS>
