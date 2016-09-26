bac0c5cfca1759fbaaede95006fbbf51bd95f248
石博文 <sbw@sbw.so>
2016-09-26 13:45:19 +0800

    update trans
    Change-Id: Ib3df62fe6bd8f62f184f4bddc55cd89fdf66e516

--- translations/dde-dock.ts
+++ translations/dde-dock.ts
@@ -4,7 +4,12 @@
 <context>
     <name>DatetimePlugin</name>
     <message>
-        <location filename="../plugins/datetime/datetimeplugin.cpp" line="73"/>
+        <location filename="../plugins/datetime/datetimeplugin.cpp" line="80"/>
+        <source>Time Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/datetime/datetimeplugin.cpp" line="105"/>
         <source> HH:mm:ss</source>
         <translation type="unfinished"></translation>
     </message>
@@ -18,14 +23,32 @@
     </message>
 </context>
 <context>
+    <name>DeviceItem</name>
+    <message>
+        <location filename="../plugins/network/item/deviceitem.cpp" line="35"/>
+        <source>Enable network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/deviceitem.cpp" line="37"/>
+        <source>Disable network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/deviceitem.cpp" line="43"/>
+        <source>Network settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>DiskControlItem</name>
     <message>
-        <location filename="../plugins/disk-mount/diskcontrolitem.cpp" line="86"/>
+        <location filename="../plugins/disk-mount/diskcontrolitem.cpp" line="87"/>
         <source>Unknown device</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugins/disk-mount/diskcontrolitem.cpp" line="92"/>
+        <location filename="../plugins/disk-mount/diskcontrolitem.cpp" line="93"/>
         <source>Unknown volume</source>
         <translation type="unfinished"></translation>
     </message>
@@ -33,8 +56,18 @@
 <context>
     <name>DiskMountPlugin</name>
     <message>
-        <location filename="../plugins/disk-mount/diskmountplugin.cpp" line="14"/>
+        <location filename="../plugins/disk-mount/diskmountplugin.cpp" line="17"/>
         <source>Disk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/disk-mount/diskmountplugin.cpp" line="67"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/disk-mount/diskmountplugin.cpp" line="73"/>
+        <source>Unmount all</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -132,27 +165,27 @@
 <context>
     <name>PopupControlWidget</name>
     <message>
-        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="68"/>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="63"/>
         <source>Empty</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="68"/>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="63"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="73"/>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="68"/>
         <source>This action cannot be restored</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="75"/>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="70"/>
         <source>Are you sure to empty 1 item ?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="77"/>
+        <location filename="../plugins/trash/popupcontrolwidget.cpp" line="72"/>
         <source>Are you sure to empty %1 items ?</source>
         <translation type="unfinished"></translation>
     </message>
@@ -161,7 +194,48 @@
     <name>ShutdownPlugin</name>
     <message>
         <location filename="../plugins/shutdown/shutdownplugin.cpp" line="47"/>
+        <location filename="../plugins/shutdown/shutdownplugin.cpp" line="95"/>
         <source>Shut down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/shutdown/shutdownplugin.cpp" line="54"/>
+        <source>Remaining Capacity %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/shutdown/shutdownplugin.cpp" line="58"/>
+        <source>Charged %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/shutdown/shutdownplugin.cpp" line="60"/>
+        <source>Charging %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/shutdown/shutdownplugin.cpp" line="101"/>
+        <source>Restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/shutdown/shutdownplugin.cpp" line="107"/>
+        <source>Log out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/shutdown/shutdownplugin.cpp" line="113"/>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/shutdown/shutdownplugin.cpp" line="121"/>
+        <source>Switch account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/shutdown/shutdownplugin.cpp" line="131"/>
+        <source>Power settings</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -179,14 +253,58 @@
     </message>
 </context>
 <context>
-    <name>TrashWidget</name>
+    <name>SoundItem</name>
     <message>
-        <location filename="../plugins/trash/trashwidget.cpp" line="17"/>
-        <source>Run</source>
+        <location filename="../plugins/sound/sounditem.cpp" line="53"/>
+        <source>Unmute</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugins/trash/trashwidget.cpp" line="18"/>
+        <location filename="../plugins/sound/sounditem.cpp" line="55"/>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/sound/sounditem.cpp" line="61"/>
+        <source>Audio Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/sound/sounditem.cpp" line="172"/>
+        <source>Current Volume %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SystemTrayPlugin</name>
+    <message>
+        <location filename="../plugins/system-tray/systemtrayplugin.cpp" line="23"/>
+        <source>System Tray</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrashPlugin</name>
+    <message>
+        <location filename="../plugins/trash/trashplugin.cpp" line="39"/>
+        <source>Trash-%1 file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/trash/trashplugin.cpp" line="41"/>
+        <source>Trash-%1 files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrashWidget</name>
+    <message>
+        <location filename="../plugins/trash/trashwidget.cpp" line="45"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/trash/trashwidget.cpp" line="53"/>
         <source>Empty</source>
         <translation type="unfinished"></translation>
     </message>
@@ -194,18 +312,31 @@
 <context>
     <name>WiredItem</name>
     <message>
-        <location filename="../plugins/network/item/wireditem.cpp" line="36"/>
+        <location filename="../plugins/network/item/wireditem.cpp" line="47"/>
         <source>Unknow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugins/network/item/wireditem.cpp" line="41"/>
-        <source>Disconnect</source>
+        <location filename="../plugins/network/item/wireditem.cpp" line="52"/>
+        <source>No Network</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugins/network/item/wireditem.cpp" line="51"/>
-        <source>Wired connection %1</source>
+        <location filename="../plugins/network/item/wireditem.cpp" line="62"/>
+        <source>Wired connection: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WirelessItem</name>
+    <message>
+        <location filename="../plugins/network/item/wirelessitem.cpp" line="54"/>
+        <source>No Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugins/network/item/wirelessitem.cpp" line="64"/>
+        <source>Wireless Connection: %1</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
