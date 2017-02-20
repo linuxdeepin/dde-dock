b0c14898f389288f00a91404b204b3fb82191c31
石博文 <sbw@sbw.so>
2017-02-20 17:41:28 +0800

    Fix dock app icon error if encoding with base64
    Change-Id: I5f90b43b4f8c525d391058a4ccb836253645cc23

--- frame/util/themeappicon.cpp
+++ frame/util/themeappicon.cpp
@@ -1,6 +1,8 @@
 #include "themeappicon.h"
 
 #include <QIcon>
+#include <QFile>
+#include <QDebug>
 
 ThemeAppIcon::ThemeAppIcon(QObject *parent) : QObject(parent)
 {
@@ -14,12 +16,23 @@ ThemeAppIcon::~ThemeAppIcon()
 
 QPixmap ThemeAppIcon::getIcon(const QString iconName, const int size)
 {
-    QIcon icon = QIcon::fromTheme(iconName, QIcon::fromTheme("application-x-desktop"));
-    QPixmap pix = icon.pixmap(QSize(size, size));
-    if (pix.isNull()) {
-        pix = QPixmap(":/icons/resources/application-x-desktop.svg").scaled(size, size);
+    QPixmap pixmap(size, size);
+
+    if (QFile::exists(iconName))
+        return QPixmap(iconName);
+    if (iconName.startsWith("data:image/"))
+    {
+        const QStringList strs = iconName.split("base64,");
+        if (strs.size() == 2)
+            pixmap.loadFromData(QByteArray::fromBase64(strs.at(1).toLatin1()));
+    } else {
+        const QIcon icon = QIcon::fromTheme(iconName, QIcon::fromTheme("application-x-desktop"));
+        pixmap = icon.pixmap(QSize(size, size));
     }
 
-    return pix;
+    if (pixmap.isNull())
+        pixmap = QPixmap(":/icons/resources/application-x-desktop.svg");
+
+    return pixmap.scaled(size, size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
 }
 
