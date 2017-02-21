26f189d5a8f6da621aabf4e93839911ace943bbe
石博文 <sbw@sbw.so>
2017-02-21 11:40:16 +0800

    Fix dock app icon is empty or size error
    Change-Id: Ifb6a383ffcb11cc67c87e83cf952df124b4af452

--- frame/util/themeappicon.cpp
+++ frame/util/themeappicon.cpp
@@ -14,27 +14,41 @@ ThemeAppIcon::~ThemeAppIcon()
 
 }
 
-QPixmap ThemeAppIcon::getIcon(const QString iconName, const int size)
+const QPixmap ThemeAppIcon::getIcon(const QString iconName, const int size)
 {
-    QPixmap pixmap(size, size);
+    QPixmap pixmap;
 
-    if (QFile::exists(iconName)) {
-        pixmap = QPixmap(iconName);
-        if (!pixmap.isNull())
-            return pixmap;
-    }
-    if (iconName.startsWith("data:image/"))
-    {
-        const QStringList strs = iconName.split("base64,");
-        if (strs.size() == 2)
-            pixmap.loadFromData(QByteArray::fromBase64(strs.at(1).toLatin1()));
-    } else {
+    do {
+
+        if (iconName.startsWith("data:image/"))
+        {
+            const QStringList strs = iconName.split("base64,");
+            if (strs.size() == 2)
+                pixmap.loadFromData(QByteArray::fromBase64(strs.at(1).toLatin1()));
+
+            if (!pixmap.isNull())
+                break;
+        }
+
+        if (QFile::exists(iconName))
+        {
+            pixmap = QPixmap(iconName);
+            if (!pixmap.isNull())
+                break;
+        }
+
         const QIcon icon = QIcon::fromTheme(iconName, QIcon::fromTheme("application-x-desktop"));
         pixmap = icon.pixmap(QSize(size, size));
-    }
+        if (!pixmap.isNull())
+            break;
 
-    if (pixmap.isNull())
         pixmap = QPixmap(":/icons/resources/application-x-desktop.svg");
+        if (!pixmap.isNull())
+            break;
+
+        Q_UNREACHABLE();
+
+    } while (false);
 
     return pixmap.scaled(size, size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
 }
