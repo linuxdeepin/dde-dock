389b1f4b63be791da2d60f5395c6bfaad5cf38e9
石博文 <sbw@sbw.so>
2017-03-07 15:51:00 +0800

    Fix plugins item not delete
    Change-Id: I8dae4579373e1e41b15607a3d197ec2fa42e5665

--- frame/controller/dockpluginscontroller.cpp
+++ frame/controller/dockpluginscontroller.cpp
@@ -55,7 +55,7 @@ void DockPluginsController::itemRemoved(PluginsItemInterface * const itemInter, 
     emit pluginItemRemoved(item);
 
     m_pluginList[itemInter].remove(itemKey);
-    item->deleteLater();
+    QTimer::singleShot(1, item, &PluginsItem::deleteLater);
 }
 
 //void DockPluginsController::requestRefershWindowVisible()
