6f338bc7e83c95a4d0995ae5dda36b3dd342918b
jouyouyun <jouyouwen717@gmail.com>
2017-09-18 13:28:56 +0800

    Update license
    Change-Id: I2e7e9a64a8481fbb91dff3ddca30a4bbace74c26

--- network/nm_setting_vpn_strongswan.go
+++ network/nm_setting_vpn_strongswan.go
@@ -1,13 +1,15 @@
-/**
- * Copyright (c) 2016 Deepin, Inc.
+/*
+ * Copyright (C) 2016 ~ 2017 Deepin Technology Co., Ltd.
  *
- * Author:      Xu FaSheng <fasheng.xu@gmail.com>
- * Maintainer:  Xu FaSheng <fasheng.xu@gmail.com>
+ * Author:     jouyouyun <jouyouwen717@gmail.com>
+ *             Xu FaSheng <fasheng.xu@gmail.com>
  *
- * This program is free software; you can redistribute it and/or modify
+ * Maintainer: Xu FaSheng <fasheng.xu@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,8 +17,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- **/
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package network
 
