133b711d1fec55f3f0555b070bc66129f201a4a5
石博文 <sbw@sbw.so>
2018-02-07 11:52:47 +0800

    update license
    Change-Id: I9df92e43b79f7c2b3688b595f80df7b3a7bb7ed2

--- frame/dbus/dbusmenumanager.cpp
+++ frame/dbus/dbusmenumanager.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 ~ 2017 Deepin Technology Co., Ltd.
+ * Copyright (C) 2015 ~ 2018 Deepin Technology Co., Ltd.
  *
  * Author:     sbw <sbw@sbw.so>
  *
--- frame/item/dockitem.cpp
+++ frame/item/dockitem.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 ~ 2017 Deepin Technology Co., Ltd.
+ * Copyright (C) 2011 ~ 2018 Deepin Technology Co., Ltd.
  *
  * Author:     sbw <sbw@sbw.so>
  *
