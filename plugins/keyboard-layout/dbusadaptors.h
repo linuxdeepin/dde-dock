3d19f67ca3b4f016d317b6102fb1c36673f2e95d
rekols <rekols@foxmail.com>
2018-03-07 13:50:16 +0800

    feat: add keyboard layout plugin.
    Change-Id: I2512d58b74b99871766551d85c874bd6d266a30e

---
+++ plugins/keyboard-layout/dbusadaptors.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2017 ~ 2018 Deepin Technology Co., Ltd.
+ *
+ * Author:     rekols <rekols@foxmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DBUSADAPTORS_H
+#define DBUSADAPTORS_H
+
+#include <QtDBus/QtDBus>
+
+class DBusAdaptors : public QDBusAbstractAdaptor
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "com.deepin.dde.Keyboard")
+    Q_CLASSINFO("D-Bus Introspection", ""
+                "  <interface name=\"com.deepin.dde.Keyboard\">\n"
+                "    <property access=\"read\" type=\"s\" name=\"layout\"/>\n"
+                "    <signal name=\"layoutChanged\">"
+                "        <arg name=\"layout\" type=\"s\"/>"
+                "    </signal>"
+                "  </interface>\n"
+                "")
+
+public:
+     DBusAdaptors(QObject *parent = nullptr);
+    ~DBusAdaptors();
+
+public:
+    Q_PROPERTY(QString layout READ layout NOTIFY layoutChanged)
+    QString layout() const;
+
+signals:
+    void layoutChanged(QString text);
+};
+
+#endif
---
+++ plugins/keyboard-layout/keyboardplugin.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2017 ~ 2018 Deepin Technology Co., Ltd.
+ *
+ * Author:     rekols <rekols@foxmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "keyboardplugin.h"
+
+KeyboardPlugin::KeyboardPlugin(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+KeyboardPlugin::~KeyboardPlugin()
+{
+}
+
+const QString KeyboardPlugin::pluginName() const
+{
+    return "keyboard";
+}
+
+const QString KeyboardPlugin::pluginDisplayName() const
+{
+    return "Keyboard";
+}
+
+void KeyboardPlugin::init(PluginProxyInterface *proxyInter)
+{
+    m_proxyInter = proxyInter;
+    m_dbusAdaptors = new DBusAdaptors(this);
+
+    QDBusConnection::sessionBus().registerService("com.deepin.dde.Keyboard");
+    QDBusConnection::sessionBus().registerObject("/com/deepin/dde/Keyboard", "com.deepin.dde.Keyboard", this);
+}
+
+QWidget* KeyboardPlugin::itemWidget(const QString &itemKey)
+{
+    Q_UNUSED(itemKey);
+
+    return nullptr;
+}
+
+QWidget* KeyboardPlugin::itemTipsWidget(const QString &itemKey)
+{
+    Q_UNUSED(itemKey);
+
+    return nullptr;
+}
