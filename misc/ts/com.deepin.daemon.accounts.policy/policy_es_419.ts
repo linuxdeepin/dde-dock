315643d0a090872bef05482f04c7c3fe6c96c695
transifex <transifex@linuxdeepin.com>
2018-03-22 01:49:41 +0000

    auto sync po files from transifex
    Change-Id: If7c6e4714d99cc1238d68f72dbe605e4f9ed88aa

--- misc/ts/com.deepin.daemon.accounts.policy/policy_es_419.ts
+++ misc/ts/com.deepin.daemon.accounts.policy/policy_es_419.ts
@@ -24,42 +24,42 @@
 		<message>
 			<location filename="com.deepin.daemon.accounts.enable-auto-login!message" line="0"/>
 			<source>Authentication is required to enable auto login</source>
-			<translation type="unfinished"/>
+			<translation>Se requiere autenticación para habilitar inicio de sesión automático</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.accounts.enable-auto-login!description" line="0"/>
 			<source>Enable Auto Login</source>
-			<translation type="unfinished"/>
+			<translation>Habilitar inicio de sesión automático</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.accounts.disable-auto-login!message" line="0"/>
 			<source>Authentication is required to disable auto login</source>
-			<translation type="unfinished"/>
+			<translation>Se requiere autenticación para deshabilitar inicio de sesión automático</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.accounts.disable-auto-login!description" line="0"/>
 			<source>Disable Auto Login</source>
-			<translation type="unfinished"/>
+			<translation>Deshabilitar inicio de sesión automático</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.accounts.enable-nopass-login!message" line="0"/>
 			<source>Authentication is required to enable login without password</source>
-			<translation type="unfinished"/>
+			<translation>Se requiere autenticación para habilitar inicio de sesión sin contraseña</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.accounts.enable-nopass-login!description" line="0"/>
 			<source>Enable Login without password</source>
-			<translation type="unfinished"/>
+			<translation>Habilitar inicio de sesión sin contraseña</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.accounts.disable-nopass-login!message" line="0"/>
 			<source>Authentication is required to disable login without password</source>
-			<translation type="unfinished"/>
+			<translation>Se requiere autenticación para deshabilitar inicio de sesión sin contraseña</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.accounts.disable-nopass-login!description" line="0"/>
 			<source>Disable Login without password</source>
-			<translation type="unfinished"/>
+			<translation>Deshabilitar inicio de sesión sin contraseña</translation>
 		</message>
 	</context>
 </TS>
