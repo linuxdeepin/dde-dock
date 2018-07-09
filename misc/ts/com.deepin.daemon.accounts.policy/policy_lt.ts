74930d56e56ab4a3c93467fa8a9bd4cff882d193
transifex <transifex@linuxdeepin.com>
2018-07-09 10:01:09 +0000

    auto sync po files from transifex
    Change-Id: Ia86662edce14510280e7fbea06a0f629e7204d40

---
+++ misc/ts/com.deepin.daemon.accounts.policy/policy_lt.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt" version="2.1">
+	<context>
+		<name>policy</name>
+		<message>
+			<location filename="com.deepin.daemon.accounts.change-own-user-data!message" line="0"/>
+			<source>Authentication is required to change your own user data</source>
+			<translation>Norint keisti asmeninio naudotojo duomenis, reikalingas tapatybės nustatymas</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.change-own-user-data!description" line="0"/>
+			<source>Change your own user data</source>
+			<translation>Keisti asmeninio naudotojo duomenis</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.user-administration!message" line="0"/>
+			<source>Authentication is required to change user data</source>
+			<translation>Norint keisti naudotojo duomenis, reikalingas tapatybės nustatymas</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.user-administration!description" line="0"/>
+			<source>Manage user accounts</source>
+			<translation>Tvarkyti naudotojų paskyras</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.enable-auto-login!message" line="0"/>
+			<source>Authentication is required to enable auto login</source>
+			<translation>Norint įjungti automatinį prisijungimą, reikalingas tapatybės nustatymas</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.enable-auto-login!description" line="0"/>
+			<source>Enable Auto Login</source>
+			<translation>Įjungti automatinį prisijungimą</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.disable-auto-login!message" line="0"/>
+			<source>Authentication is required to disable auto login</source>
+			<translation>Norint išjungti automatinį prisijungimą, reikalingas tapatybės nustatymas</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.disable-auto-login!description" line="0"/>
+			<source>Disable Auto Login</source>
+			<translation>Išjungti automatinį prisijungimą</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.enable-nopass-login!message" line="0"/>
+			<source>Authentication is required to enable login without password</source>
+			<translation>Norint įjungti prisijungimą be slaptažodžio, reikalingas tapatybės nustatymas</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.enable-nopass-login!description" line="0"/>
+			<source>Enable Login without password</source>
+			<translation>Įjungti prisijungimą be slaptažodžio</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.disable-nopass-login!message" line="0"/>
+			<source>Authentication is required to disable login without password</source>
+			<translation>Norint išjungti prisijungimą be slaptažodžio, reikalingas tapatybės nustatymas</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.accounts.disable-nopass-login!description" line="0"/>
+			<source>Disable Login without password</source>
+			<translation>Išjungti prisijungimą be slaptažodžio</translation>
+		</message>
+	</context>
+</TS>
