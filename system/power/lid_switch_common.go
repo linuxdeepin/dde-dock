22b6c990371978603d1bd84d78d57ced1106c1e0
electricface <songwentai@deepin.com>
2018-09-12 16:09:36 +0800

    fix(system/power): lid switch not found
    Change-Id: I3befbcec85d2b20bc3fe48090c5e9433ab224a57

--- system/power/lid_switch_common.go
+++ system/power/lid_switch_common.go
@@ -19,15 +19,74 @@
 
 package power
 
+/*
+#include <linux/input.h>
+*/
+import "C"
+
 import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"syscall"
 	"unsafe"
 )
+
+const (
+	SW_LID = C.SW_LID
+	SW_CNT = C.SW_CNT
+	SW_MAX = C.SW_MAX
+	EV_SW  = C.EV_SW
+
+	bitsPerLong = int(unsafe.Sizeof(C.long(0))) * 8
+)
+
+// NBITS(x) ((((x)-1)/BITS_PER_LONG)+1)
+func NBITS(x int) int {
+	return (((x) - 1) / bitsPerLong) + 1
+}
+
+// #define LONG(x) ((x)/BITS_PER_LONG)
+func LONG(x int) int {
+	return x / bitsPerLong
+}
+
+// #define OFF(x)  ((x)%BITS_PER_LONG)
+func OFF(x int) int {
+	return x % bitsPerLong
+}
+
+//#define test_bit(bit, array)    ((array[LONG(bit)] >> OFF(bit)) & 1)
+func testBit(bit int, array []int) bool {
+	v := (array[LONG(bit)] >> uint(OFF(bit))) & 1
+	if v == 0 {
+		return false
+	}
+	return true
+}
+
+func upInputStrToBitmask(s string, bitmask []int) int {
+	var numBitsSet int
+	maxSize := len(bitmask)
+	v := strings.SplitN(s, " ", maxSize)
+
+	j := 0
+	for i := len(v) - 1; i >= 0; i-- {
+		val, _ := strconv.ParseUint(v[i], 16, 64)
+		bitmask[j] = int(val)
+
+		for val != 0 {
+			numBitsSet++
+			val &= val - 1
+		}
+
+		j++
+	}
+	return numBitsSet
+}
 
 type InputEvent struct {
 	Time  syscall.Timeval // time in seconds since epoch at which event occurred
@@ -43,25 +102,42 @@ func (ev *InputEvent) String() string {
 		ev.Time.Sec, ev.Time.Usec, ev.Code, ev.Type, ev.Value)
 }
 
-var eventSize = int(unsafe.Sizeof(InputEvent{}))
+const eventSize = int(unsafe.Sizeof(InputEvent{}))
 
 func (m *Manager) findLidSwitch() string {
 	devices := m.gudevClient.QueryBySubsystem("input")
-	var deviceFile string
+
+	defer func() {
+		// free devices
+		for _, device := range devices {
+			device.Unref()
+		}
+	}()
+
 	for _, device := range devices {
 		name := device.GetName()
 		if !strings.HasPrefix(name, "event") {
 			continue
 		}
-		logger.Debug("name:", name)
-		logger.Debug("sysfsPath:", device.GetSysfsPath())
-		devName := device.GetSysfsAttr("device/name")
-		logger.Debugf("dev name: %q", devName)
-		if strings.Contains(strings.ToLower(devName), "lid switch") {
-			deviceFile = device.GetDeviceFile()
-			if deviceFile != "" {
-				return deviceFile
-			}
+
+		capSw := device.GetSysfsAttr("../capabilities/sw")
+
+		bitmask := make([]int, NBITS(SW_LID))
+		numBits := upInputStrToBitmask(capSw, bitmask)
+
+		if numBits == 0 || numBits >= SW_CNT {
+			// invalid bitmask entry
+			continue
+		}
+
+		if !testBit(SW_LID, bitmask) {
+			// not a lid
+			continue
+		}
+
+		deviceFile := device.GetDeviceFile()
+		if deviceFile != "" {
+			return deviceFile
 		}
 	}
 	return ""
@@ -109,11 +185,6 @@ func (m *Manager) initLidSwitchCommon() {
 		}
 	}()
 }
-
-const (
-	EV_SW  = 0x05
-	SW_LID = 0x00
-)
 
 func readLidSwitchEvent(f *os.File) ([]InputEvent, error) {
 	// read
