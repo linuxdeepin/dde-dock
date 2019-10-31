a2acf8ff06f8f0b58fe3cda8d3fda7ebf4a3d533
electricface <songwentai@deepin.com>
2019-10-31 10:57:42 +0800

    chore(keybinding): execCmdInterval check only works on specific shortcuts

--- keybinding/manager_handlers.go
+++ keybinding/manager_handlers.go
@@ -42,14 +42,18 @@ func (m *Manager) initHandlers() {
 		// prevent shortcuts such as switch window managers from being
 		// triggered twice by mistake.
 		const minExecCmdInterval = 600 * time.Millisecond
-		now := time.Now()
-		duration := now.Sub(m.lastExecCmdTime)
-		logger.Debug("handle ActionTypeExecCmd duration:", duration)
-		if 0 < duration && duration < minExecCmdInterval {
-			logger.Debug("handle ActionTypeExecCmd ignore key event")
-			return
+		type0 := ev.Shortcut.GetType()
+		id := ev.Shortcut.GetId()
+		if type0 == ShortcutTypeSystem && id == "wm-switcher" {
+			now := time.Now()
+			duration := now.Sub(m.lastExecCmdTime)
+			logger.Debug("handle ActionTypeExecCmd duration:", duration)
+			if 0 < duration && duration < minExecCmdInterval {
+				logger.Debug("handle ActionTypeExecCmd ignore key event")
+				return
+			}
+			m.lastExecCmdTime = now
 		}
-		m.lastExecCmdTime = now
 
 		action := ev.Shortcut.GetAction()
 		arg, ok := action.Arg.(*ActionExecCmdArg)
