e886b2f3707e0f622ac98635d92371eecdfce76b
shaojun <wangshaojun_cm@deepin.com>
2019-12-04 13:28:07 +0800

    fixed(network):open control center network config

--- plugins/network/item/applet/wirelesslist.cpp
+++ plugins/network/item/applet/wirelesslist.cpp
@@ -417,7 +417,7 @@ void WirelessList::onActivateApFailed(const QString &apPath, const QString &uuid
                 .path("/com/deepin/dde/ControlCenter")
                 .method("ShowPage")
                 .arg(QString("network"))
-                .arg(apPath)
+                .arg(QString("%1,%2").arg(m_device->path()).arg(apPath))
                 .call();
 
         Q_EMIT requestSetAppletVisible(false);
