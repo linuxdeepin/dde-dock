6aa3d4be5feb238240397ff9fd4b574e85e46494
quezhiyong <quezhiyong@uniontech.com>
2020-04-08 17:13:31 +0800

    chore(polkit-action): add rename-fingerprint action

--- misc/ts/com.deepin.daemon.Fprintd.policy/policy.ts
+++ misc/ts/com.deepin.daemon.Fprintd.policy/policy.ts
@@ -1,25 +1,37 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.1">
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
-			<location filename="com.deepin.daemon.fprintd.enroll!message" line="0"/>
+			<location filename="com.deepin.daemon.fprintd.enroll!message" line="0" />
 			<source>Authentication is required to add fingerprint password</source>
-			<translation>Authentication is required to add fingerprint password</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.fprintd.enroll!description" line="0"/>
+			<location filename="com.deepin.daemon.fprintd.enroll!description" line="0" />
 			<source>Add fingerprint password</source>
-			<translation>Add fingerprint password</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.fprintd.delete-enrolled-fingers!message" line="0"/>
+			<location filename="com.deepin.daemon.fprintd.delete-enrolled-fingers!message" line="0" />
 			<source>Authentication is required to clear fingerprint passwords</source>
-			<translation>Authentication is required to clear fingerprint passwords</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.fprintd.delete-enrolled-fingers!description" line="0"/>
+			<location filename="com.deepin.daemon.fprintd.delete-enrolled-fingers!description" line="0" />
 			<source>Clear fingerprint passwords</source>
-			<translation>Clear fingerprint passwords</translation>
+			<translation type="unfinished" />
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.fprintd.rename-enrolled-finger!message" line="0" />
+			<source>Authentication is required to rename fingerprint password</source>
+			<translation type="unfinished" />
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.fprintd.rename-enrolled-finger!description" line="0" />
+			<source>Rename fingerprint password</source>
+			<translation type="unfinished" />
 		</message>
 	</context>
 </TS>
