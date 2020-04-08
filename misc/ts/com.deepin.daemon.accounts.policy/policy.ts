1478b8d4798c1980ad11cabe4c3c7268bd51b66a
quezhiyong <quezhiyong@uniontech.com>
2020-04-08 17:13:31 +0800

    chore(polkit-action): add rename-fingerprint action

--- misc/ts/com.deepin.daemon.accounts.policy/policy.ts
+++ misc/ts/com.deepin.daemon.accounts.policy/policy.ts
@@ -1,75 +1,77 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.1">
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
-			<location filename="com.deepin.daemon.accounts.change-own-user-data!message" line="0"/>
+			<location filename="com.deepin.daemon.accounts.change-own-user-data!message" line="0" />
 			<source>Authentication is required to change your own user data</source>
-			<translation>Authentication is required to change your own user data</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.change-own-user-data!description" line="0"/>
+			<location filename="com.deepin.daemon.accounts.change-own-user-data!description" line="0" />
 			<source>Change your own user data</source>
-			<translation>Change your own user data</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.user-administration!message" line="0"/>
+			<location filename="com.deepin.daemon.accounts.user-administration!message" line="0" />
 			<source>Authentication is required to change user data</source>
-			<translation>Authentication is required to change user data</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.user-administration!description" line="0"/>
+			<location filename="com.deepin.daemon.accounts.user-administration!description" line="0" />
 			<source>Manage user accounts</source>
-			<translation>Manage user accounts</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.enable-auto-login!message" line="0"/>
+			<location filename="com.deepin.daemon.accounts.enable-auto-login!message" line="0" />
 			<source>Authentication is required to enable auto login</source>
-			<translation>Authentication is required to enable auto login</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.enable-auto-login!description" line="0"/>
+			<location filename="com.deepin.daemon.accounts.enable-auto-login!description" line="0" />
 			<source>Enable Auto Login</source>
-			<translation>Enable Auto Login</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.disable-auto-login!message" line="0"/>
+			<location filename="com.deepin.daemon.accounts.disable-auto-login!message" line="0" />
 			<source>Authentication is required to disable auto login</source>
-			<translation>Authentication is required to disable auto login</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.disable-auto-login!description" line="0"/>
+			<location filename="com.deepin.daemon.accounts.disable-auto-login!description" line="0" />
 			<source>Disable Auto Login</source>
-			<translation>Disable Auto Login</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.enable-nopass-login!message" line="0"/>
+			<location filename="com.deepin.daemon.accounts.enable-nopass-login!message" line="0" />
 			<source>Authentication is required to enable login without password</source>
-			<translation>Authentication is required to enable login without password</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.enable-nopass-login!description" line="0"/>
+			<location filename="com.deepin.daemon.accounts.enable-nopass-login!description" line="0" />
 			<source>Enable Login without password</source>
-			<translation>Enable Login without password</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.disable-nopass-login!message" line="0"/>
+			<location filename="com.deepin.daemon.accounts.disable-nopass-login!message" line="0" />
 			<source>Authentication is required to disable login without password</source>
-			<translation>Authentication is required to disable login without password</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.disable-nopass-login!description" line="0"/>
+			<location filename="com.deepin.daemon.accounts.disable-nopass-login!description" line="0" />
 			<source>Disable Login without password</source>
-			<translation>Disable Login without password</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.set-keyboard-layout!message" line="0"/>
+			<location filename="com.deepin.daemon.accounts.set-keyboard-layout!message" line="0" />
 			<source>Authentication is required to set keyboard layout</source>
-			<translation>Authentication is required to set keyboard layout</translation>
+			<translation type="unfinished" />
 		</message>
 		<message>
-			<location filename="com.deepin.daemon.accounts.set-keyboard-layout!description" line="0"/>
+			<location filename="com.deepin.daemon.accounts.set-keyboard-layout!description" line="0" />
 			<source>Set keyboard layout</source>
-			<translation>Set keyboard layout</translation>
+			<translation type="unfinished" />
 		</message>
 	</context>
 </TS>
