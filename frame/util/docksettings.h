634265aec4cf351c5a112f95e14c445fb68817ab
zhaolong <zhaolong@deepin.com>
2020-04-21 17:47:35 +0800

    feat(frame):multi-screen down taskbar location
    多屏扩展模式下桌面任务栏显示,在调整屏幕拼接位置或多屏扩展模式时下dock位置与以相应变化 task:18769 18771
    
    (cherry picked from commit 5eb654e1c7f3349a1be6230a535cedc01b9d0540)
    
    # Conflicts:
    #	frame/util/docksettings.h
    #	frame/window/mainwindow.cpp
    #	frame/window/mainwindow.h

--- frame/util/docksettings.h
+++ frame/util/docksettings.h
@@ -23,11 +23,10 @@
 #define DOCKSETTINGS_H
 
 #include "constants.h"
-#include "dbus/dbusmenumanager.h"
-#include "dbus/dbusdisplay.h"
-#include "controller/dockitemmanager.h"
+#include "monitor.h"
 
 #include <com_deepin_dde_daemon_dock.h>
+#include <com_deepin_daemon_display.h>
 
 #include <QAction>
 #include <QMenu>
@@ -35,13 +34,11 @@
 #include <QObject>
 #include <QSize>
 
-#include <QStyleFactory>
-
-DWIDGET_USE_NAMESPACE
-
 using namespace Dock;
 using DBusDock = com::deepin::dde::daemon::Dock;
+using DisplayInter = com::deepin::daemon::Display;
 
+class DockItemManager;
 class DockSettings : public QObject
 {
     Q_OBJECT
@@ -59,7 +56,10 @@ public:
     inline int narrowTimeout() const { return 100; }
     inline bool autoHide() const { return m_autoHide; }
     const QRect primaryRect() const;
+    const QRect currentRect() const;
+    const QList<QRect> monitorsRect() const;
     inline const QRect primaryRawRect() const { return m_primaryRawRect; }
+    inline const QRect currentRawRect() const { return m_currentRawRect; }
     inline const QRect frontendWindowRect() const { return m_frontendRect; }
     inline const QSize windowSize() const { return m_mainWindowSize; }
     inline const quint8 Opacity() const { return m_opacity * 255; }
@@ -101,21 +101,28 @@ private slots:
     void dockItemCountChanged();
     void primaryScreenChanged();
     void resetFrontendGeometry();
-    void updateForbidPostions();
     void onOpacityChanged(const double value);
     void trayVisableCountChanged(const int &count);
     void onWindowSizeChanged();
     void onTrashGSettingsChanged(const QString &key);
+    void onMonitorListChanged(const QList<QDBusObjectPath> &mons);
 
 private:
-    DockSettings(QWidget *parent = 0);
+    DockSettings(QWidget *parent = nullptr);
     DockSettings(DockSettings const &) = delete;
     DockSettings operator =(DockSettings const &) = delete;
 
-    bool test(const Position pos, const QList<QRect> &otherScreens) const;
     void calculateWindowConfig();
     void gtkIconThemeChanged();
     void checkService();
+
+    void calculateMultiScreensPos();
+    void monitorAdded(const QString &path);
+    void monitorRemoved(const QString &path);
+    void twoScreensCalPos();
+    void treeScreensCalPos();
+    void combination(QList<Monitor*> &screens);
+    void calculateRelativePos(Monitor *s1, Monitor *s2);
 
 private:
     int m_dockWindowSize;
@@ -130,6 +137,7 @@ private:
     HideState m_hideState;
     DisplayMode m_displayMode;
     QRect m_primaryRawRect;
+    mutable QRect m_currentRawRect;
     QRect m_frontendRect;
 
     QMenu m_settingsMenu;
@@ -144,9 +152,11 @@ private:
     QAction m_keepHiddenAct;
     QAction m_smartHideAct;
 
-    DBusDisplay *m_displayInter;
+    DisplayInter *m_displayInter;
     DockItemManager *m_itemManager;
     bool m_trashPluginShow;
+
+    QMap<Monitor *, MonitorInter *> m_monitors;
 };
 
 #endif // DOCKSETTINGS_H
