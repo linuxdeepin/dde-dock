63e86ac922eab43e5c579b6655c6277a91e8e66a
zhaolong <zhaolong@deepin.com>
2020-04-27 09:40:44 +0800

    fix(network): tooltips显示空白
    当有线网络刚连接上时，其更新的连接信息未发送出去，致使tooltiops显示未更新，出现显示空的现象
    同task:20771
    
    Log: 修复网络未连接成功处于连接中时tooltip提示为空
    Task: https://pms.uniontech.com/zentao/bug-view-29885.html
    
    (cherry picked from commit eceee98ccbf58635f2fca8dd98f6b44a11bbf245)
    
    # Conflicts:
    #	plugins/network/item/wireditem.cpp

--- plugins/network/item/wireditem.cpp
+++ plugins/network/item/wireditem.cpp
@@ -232,6 +232,8 @@ void WiredItem::changedActiveWiredConnectionInfo(const QJsonObject &connInfo)
         m_connectedName->setText(m_deviceName);
     else
         m_connectedName->setText(strTitle);
+
+    emit activeConnectionChanged();
 }
 
 void WiredItem::buttonEnter()
