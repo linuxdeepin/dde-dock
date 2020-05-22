d5b88d4bce546bf9e31d0d74e46a9cb82be073fd
wubowen <wubowen_cm@deepin.com>
2020-05-22 13:25:13 +0800

    fix(languages):Update languages files

--- misc/ts/com.deepin.daemon.Fprintd.policy/policy_de.ts
+++ misc/ts/com.deepin.daemon.Fprintd.policy/policy_de.ts
@@ -4,22 +4,32 @@
 		<message>
 			<location filename="com.deepin.daemon.fprintd.enroll!message" line="0"/>
 			<source>Authentication is required to add fingerprint password</source>
-			<translation>Die Authentifizierung ist erforderlich, um ein Fingerabdruckpasswort hinzuzufügen</translation>
+			<translation>Zum Hinzufügen des Fingerabdruckpassworts ist eine Authentifizierung erforderlich</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.fprintd.enroll!description" line="0"/>
 			<source>Add fingerprint password</source>
-			<translation>Fingerabdruckpasswort</translation>
+			<translation>Fingerabdruckpasswort hinzufügen</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.fprintd.delete-enrolled-fingers!message" line="0"/>
 			<source>Authentication is required to clear fingerprint passwords</source>
-			<translation>Die Authentifizierung ist erforderlich, um Fingerabdruckpasswörter zu löschen</translation>
+			<translation>Zum Löschen von Fingerabdruckpasswörtern ist eine Authentifizierung erforderlich</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.fprintd.delete-enrolled-fingers!description" line="0"/>
 			<source>Clear fingerprint passwords</source>
 			<translation>Fingerabdruckpasswörter löschen</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.fprintd.rename-enrolled-finger!message" line="0"/>
+			<source>Authentication is required to rename fingerprint password</source>
+			<translation>Zum Umbenennen des Fingerabdruckpassworts ist eine Authentifizierung erforderlich</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.daemon.fprintd.rename-enrolled-finger!description" line="0"/>
+			<source>Rename fingerprint password</source>
+			<translation>Fingerabdruckpasswort umbenennen</translation>
 		</message>
 	</context>
 </TS>
--- misc/ts/com.deepin.daemon.Grub2.policy/policy_sk.ts
+++ misc/ts/com.deepin.daemon.Grub2.policy/policy_sk.ts
@@ -14,12 +14,12 @@
 		<message>
 			<location filename="com.deepin.daemon.grub2.prepare-gfxmode-detect!message" line="0"/>
 			<source>Authentication is required to prepare grub2 display resolution detection</source>
-			<translation>Authentication is required to prepare grub2 display resolution detection</translation>
+			<translation>Na prípravu detekcie rozlíšenia displeja grub2 je potrebné overenie</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.daemon.grub2.prepare-gfxmode-detect!description" line="0"/>
 			<source>Prepare grub2 display resolution detection</source>
-			<translation>Prepare grub2 display resolution detection</translation>
+			<translation>Pripraviť detekciu rozlíšenia obrazovky grub2</translation>
 		</message>
 	</context>
 </TS>
