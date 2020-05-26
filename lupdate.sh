a35d64c5ae37573044eaac39f2bdc579f6c96513
chenwei <chenwei@uniontech.com>
2020-05-26 10:47:37 +0800

    feat:Add a script to update the translation file
    添加更新翻译文件的脚本

--- lupdate.sh
+++ lupdate.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cp ".transifexrc" ${HOME}/
-
-lupdate ./ -ts -no-obsolete translations/dde-dock.ts
-
+rm ./translations/dde-dock.ts
+lupdate ./ -ts -no-obsolete ./translations/dde-dock.ts
 tx push -s -b maintain/20_0102
