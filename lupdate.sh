2b883b82a40202356d5d1a5256d810b4ad074530
fpc_diesel <fanpengcheng_cm@deepin.com>
2020-06-01 11:26:56 +0800

    docs(translations):add lupdate sh && update ts

--- lupdate.sh
+++ lupdate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+cp ".transifexrc" ${HOME}/
 rm ./translations/dde-dock.ts
 cp ".transifexrc" ${HOME}/
 lupdate ./ -ts -no-obsolete ./translations/dde-dock.ts
-tx push -s -b maintain/20_0102
+tx push -s -b maintain/20_0102_SP2
