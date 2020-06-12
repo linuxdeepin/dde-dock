b382f66f5cbc5137011e797a16fdbe00445aa14f
hubenchang <hubenchang@deepin.com>
2020-06-12 16:56:01 +0800

    fix(network): display IPv6 gateway
    IPv6的接口可以有不止一个地址，之前返回的网关地址中只包含了各个地址的网关，而没有包含接口的网关。
    
    Log: display IPv6 gateway
    Bug: https://pms.uniontech.com/zentao/bug-view-31836.html

--- network/utils_dbus_nm.go
+++ network/utils_dbus_nm.go
@@ -808,13 +808,20 @@ func nmGetIp6ConfigInfo(path dbus.ObjectPath) (address, prefix string, gateways,
 	}
 
 	addressProp, _ := ip6config.Addresses().Get(0)
+	gateway, _ := ip6config.Gateway().Get(0)
+	gateways = append(gateways, gateway)
 	ipv6Addresses := wrapNMDBusIpv6Addresses(addressProp)
 	if len(ipv6Addresses) > 0 {
 		address = ipv6Addresses[0].Address
 		prefix = fmt.Sprintf("%d", ipv6Addresses[0].Prefix)
 	}
-	for _, address := range ipv6Addresses {
-		gateways = append(gateways, address.Gateway)
+	for _, addr := range ipv6Addresses {
+		gateways = append(gateways, addr.Gateway)
+		if(addr.Address[:5] != "FE80:" &&     // link local
+			addr.Address[:5] != "FEC0:") {    // site local
+			address = addr.Address
+			prefix = fmt.Sprintf("%d", addr.Prefix)
+		}
 	}
 
 	nameServersProp, _ := ip6config.Nameservers().Get(0)
