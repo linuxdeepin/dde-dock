89a4d80350e72f2548d1090673fb536d5f8523d8
Lvpeilong <lvpeilong@uniontech.com>
2020-07-25 15:03:39 +0800

    fix: 判断空指针，修复任务栏重启
    未进行空指针判断，导致任务栏崩溃重启问题
    
    Log: 添加空指针判断，修复任务栏崩溃重启问题
    Bug: https://pms.uniontech.com/zentao/bug-view-39522.html
    Change-Id: I1686558280ef690a03f508283696a7b25c1ab9ea
    Reviewed-on: http://gerrit.uniontech.com/c/dde-dock/+/626
    Reviewed-by: panyangzhengqiu <panyangzhengqiu@uniontech.com>
    Tested-by: panyangzhengqiu <panyangzhengqiu@uniontech.com>

--- plugins/tray/system-trays/systemtrayitem.cpp
+++ plugins/tray/system-trays/systemtrayitem.cpp
@@ -137,13 +137,15 @@ void SystemTrayItem::sendClick(uint8_t mouseButton, int x, int y)
 
 QWidget *SystemTrayItem::trayTipsWidget()
 {
-    m_pluginInter->itemTipsWidget(m_itemKey)->setAccessibleName(m_pluginInter->pluginName());
+    if(nullptr != m_pluginInter->itemTipsWidget(m_itemKey))
+        m_pluginInter->itemTipsWidget(m_itemKey)->setAccessibleName(m_pluginInter->pluginName());
     return m_pluginInter->itemTipsWidget(m_itemKey);
 }
 
 QWidget *SystemTrayItem::trayPopupApplet()
 {
-    m_pluginInter->itemPopupApplet(m_itemKey)->setAccessibleName(m_pluginInter->pluginName());
+    if(nullptr != m_pluginInter->itemPopupApplet(m_itemKey))
+        m_pluginInter->itemPopupApplet(m_itemKey)->setAccessibleName(m_pluginInter->pluginName());
     return m_pluginInter->itemPopupApplet(m_itemKey);
 }
 
