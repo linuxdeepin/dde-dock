d061a2cfc8b66573c9dd46e0286f0d08f38a41ef
聂成 <niecheng@uniontech.com>
2020-08-03 14:25:19 +0800

    fix: 去除编译警告
    移除废弃的接口
    
    Log: 移除废弃的接口
    Change-Id: I8f7dd7ec8e5abfcd9fdcdc4e312ff154eead0344
    Reviewed-on: http://gerrit.uniontech.com/c/dde-dock/+/1151
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: niecheng <niecheng@uniontech.com>
    Reviewed-by: wangwei <wangwei@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- frame/main.cpp
+++ frame/main.cpp
@@ -224,7 +224,7 @@ int main(int argc, char *argv[])
     parser.addOption(disablePlugOption);
     parser.process(app);
 
-    DGuiApplicationHelper::setSingelInstanceInterval(-1);
+    DGuiApplicationHelper::setSingleInstanceInterval(-1);
     if (!app.setSingleInstance(QString("dde-dock_%1").arg(getuid()))) {
         qDebug() << "set single instance failed!";
         return -1;
