0873d91c22128856d5e68d017faa25bc6e94c1dc
hubenchang0515 <hubenchang@uniontech.com>
2020-08-21 18:02:47 +0800

    feat(inputdevices): add test case of inputdevices
    在输入设备模块中添加了utils的测试用例
    
    Log: 新增inputdevices模块测试用例
    Change-Id: I91c0eeab04213c9549665fe17042a4b500c4f946
    Reviewed-on: http://gerrit.uniontech.com/c/dde-daemon/+/2601
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: quezhiyong <quezhiyong@uniontech.com>
    Tested-by: <mailman@uniontech.com>

---
+++ inputdevices/utils_test.go
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2016 ~ 2018 Deepin Technology Co., Ltd.
+ *
+ * Author:     jouyouyun <jouyouwen717@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package inputdevices
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func Test_addItemToList(t *testing.T) {
+	Convey("addItemToList", t, func(c C) {
+		dst := []string{}
+		src := []string{
+			"hello",
+			"world",
+			"foo",
+			"bar",
+		}
+		var ret bool
+
+		for _, str := range src {
+			dst, ret = addItemToList(str, dst)
+			c.So(ret, ShouldBeTrue)
+		}
+
+		for _, str := range src {
+			dst, ret = addItemToList(str, dst)
+			c.So(ret, ShouldBeFalse)
+		}
+
+		for _, str := range src {
+			dst, ret = delItemFromList(str, dst)
+			c.So(ret, ShouldBeTrue)
+		}
+
+		for _, str := range src {
+			dst, ret = delItemFromList(str, dst)
+			c.So(ret, ShouldBeFalse)
+		}
+	})
+}
