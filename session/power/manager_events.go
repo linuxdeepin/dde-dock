2bf6b1dad78a96c53538b15e97fe758373b62125
linxin <linxin@uniontech.com>
2020-09-15 14:08:42 +0800

    fix(power): 设置笔记本合盖后不做任何操作导致未在设定时间内锁屏
    当设置笔记本合盖时不做任何操作时，合盖然后开盖，设置1分钟锁屏，此时并没有按时锁屏，此现象是因为HandleIdleOn信号被忽略导致
    
    Log: 设置笔记本合盖后不做任何操作导致未在设定时间内锁屏
    Bug: https://pms.uniontech.com/zentao/bug-view-47666.html
    Change-Id: Ia09b8b314fd581089683df6961a9aaadcadd79e1
    Reviewed-on: http://gerrit.uniontech.com/c/dde-daemon/+/4799
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: quezhiyong <quezhiyong@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- session/power/manager_events.go
+++ session/power/manager_events.go
@@ -30,9 +30,9 @@ import (
 // nolint
 const (
 	suspendStateUnknown = iota + 1
+	suspendStateLidOpen
 	suspendStateFinish
 	suspendStateWakeup
-	suspendStateLidOpen
 	suspendStatePrepare
 	suspendStateLidClose
 	suspendStateButtonClick
