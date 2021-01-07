91c16018e0b9d66a658aa2ca0b89285dd9f9c091
Zhang Qipeng <zhangqipeng@uniontech.com>
2021-01-07 21:56:53 +0800

    fix: 时间插件显示异常
    任务栏参数变化时，时间插件没有同步刷新布局。
    
    Log: 修复时间插件显示异常的问题。
    Bug: https://pms.uniontech.com/zentao/bug-view-59013.html
    Bug: https://pms.uniontech.com/zentao/bug-view-60340.html
    Bug: https://pms.uniontech.com/zentao/bug-view-59126.html
    Change-Id: I6df9a6155dcff38758ef06b2ca12826761037fab

--- frame/window/mainwindow.cpp
+++ frame/window/mainwindow.cpp
@@ -254,6 +254,7 @@ void MainWindow::resizeEvent(QResizeEvent *event)
     // 任务栏大小、位置、模式改变都会触发resize，发射大小改变信号，供依赖项目更新位置
     Q_EMIT panelGeometryChanged();
 
+    m_mainPanel->updatePluginsLayout();
     m_shadowMaskOptimizeTimer->start();
 
     return DBlurEffectWidget::resizeEvent(event);
