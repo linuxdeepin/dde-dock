d330600907750e61889c6b28260df3b1c3765870
liuxing <liuxing@uniontech.com>
2021-03-05 10:10:26 +0800

    feat: 配置ci
    文件命名、生成脚本
    
    Log: 任务栏配置ci
    Change-Id: I0948f79beb3348ee1dccb90bf4b4453d5c1e9c13

---
+++ tests/test-recoverage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+BUILD_DIR=build
+REPORT_DIR=report
+
+cd ../
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
+cmake ../
+make
+
+cd tests/
+
+lcov -c -i -d ./ -o init.info
+./dde_dock_unit_test
+lcov -c -d ./ -o cover.info
+lcov -a init.info -a cover.info -o total.info
+lcov -r total.info "*/tests/*" "*/usr/include*" "*build/src*" -o final.info
+
+rm -rf ../../tests/$REPORT_DIR
+mkdir -p ../../tests/$REPORT_DIR
+genhtml -o ../../tests/$REPORT_DIR final.info
