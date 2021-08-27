14f7544a2adec3cc81ce64be8447cc14e6300b04
FanPengCheng <fanpengcheng@uniontech.com>
2021-08-27 11:25:18 +0800

    fix: 修复任务栏图标无法重复点击的问题
    修复任务栏图标无法重复点击的问题
    
    Log:
    Change-Id: I2abd3b75c3f663b3f7c347c4650a61805b422741

--- frame/item/appitem.cpp
+++ frame/item/appitem.cpp
@@ -287,7 +287,7 @@ void AppItem::mouseReleaseEvent(QMouseEvent *e)
     }
 
     int curTimestamp = QDateTime::currentSecsSinceEpoch();
-    if ((curTimestamp - m_lastclickTimes) < 300)
+    if ((curTimestamp - m_lastclickTimes) < 1)
         return;
 
     m_lastclickTimes = curTimestamp;
