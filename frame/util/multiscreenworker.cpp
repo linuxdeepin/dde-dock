4e1546e052c7b6f08a9be428faf1cd7628f3a2e6
zengaoyuan <zengaoyuan@uniontech.com>
2021-11-12 11:22:30 +0800

    fix: 一直隐藏状态下任务栏挤占其它窗口
    一直隐藏状态下不挤占其它窗口
    
    Log: 添加过滤条件
    Influence: dde-dock显示
    Task: https://pms.uniontech.com/zentao/bug-view-101836.html
    Change-Id: Ibaa55e26bdb268e8c99abdb8e132d1baa9eac489

--- frame/util/multiscreenworker.cpp
+++ frame/util/multiscreenworker.cpp
@@ -32,6 +32,7 @@
 #include <QDBusConnection>
 
 #include <QGSettings>
+#include <QX11Info>
 
 static bool GSettingsByMouseActivationHide()
 {
@@ -747,26 +748,22 @@ void MultiScreenWorker::onRequestNotifyWindowManager()
     const auto ratio = qApp->devicePixelRatio();
     const QRect rect = getDockShowGeometry(m_ds.current(), m_position, m_displayMode);
 
+    /* 在非主屏或非一直显示状态时，清除任务栏区域，不挤占应用 */
+    if ((m_ds.current() != m_ds.primary() && m_hideMode == HideMode::KeepShowing) || m_hideMode != Dock::KeepShowing) {
+        lastRect = QRect();
+        const auto display = QX11Info::display();
+        if (!display) {
+            qWarning() << "QX11Info::display() is " << display;
+            return;
+        }
+        XcbMisc::instance()->clear_strut_partial(xcb_window_t(parent()->winId()));
+        return;
+    }
+
     // 已经设置过了，避免重复设置
     if (rect == lastRect)
         return;
     lastRect = rect;
-
-    // 先清除原先的窗管任务栏区域
-    XcbMisc::instance()->clear_strut_partial(xcb_window_t(parent()->winId()));
-
-    // 在副屏时,且为一直显示时,不要挤占应用,这是sp3的新需求
-    if (m_ds.current() != m_ds.primary() && m_hideMode == HideMode::KeepShowing) {
-        lastRect = QRect();
-        qDebug() << "don`t set dock area";
-        return;
-    }
-
-    // 除了"一直显示"模式,其他的都不要设置任务栏区域
-    if (m_hideMode != Dock::KeepShowing) {
-        lastRect = QRect();
-        return;
-    }
 
     qDebug() <<"Update Window WorkArea:" << rect;
 
