4c2b4adf30f9c5a02729d3205639a28465a59860
justforlxz <justforlxz@gmail.com>
2022-01-05 20:01:25 +0800

    fix: missing radius on high version of kwin
    don't set 0 on init
    
    Log: 修复在高版本kwin上没有圆角
    Task: https://pms.uniontech.com/zentao/task-view-97010.html
    Influence: no
    Change-Id: I0411f9452e1265b137222f34cc1e8703c9d374b2

--- frame/window/mainwindow.cpp
+++ frame/window/mainwindow.cpp
@@ -114,7 +114,6 @@ MainWindow::MainWindow(QWidget *parent)
     DPlatformWindowHandle::enableDXcbForWindow(this, true);
     m_platformWindowHandle.setEnableBlurWindow(true);
     m_platformWindowHandle.setTranslucentBackground(true);
-    m_platformWindowHandle.setWindowRadius(0);
     m_platformWindowHandle.setShadowOffset(QPoint(0, 5));
     m_platformWindowHandle.setShadowColor(QColor(0, 0, 0, 0.3 * 255));
 
@@ -660,4 +659,3 @@ void MainWindow::sendNotifications()
                 .call();
     });
 }
-
