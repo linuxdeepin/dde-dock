c4289d75983b2712259b70826540f69254d8f9c3
chenhongtao <chenhongtao@uniontech.com>
2023-01-16 17:45:35 +0800

    chore: add consts about access name
    Log: the name in the file can let appitem to define if to use the wheel
    event

---
+++ frame/item/itemconsts.h
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2011 - 2022 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef ITEMCONSTS_H
+#define ITEMCONSTS_H
+
+#include <QString>
+const QString OVERFLOWWIDGET_ACCESS_NAME = QStringLiteral("overlayScroll");
+
+#endif // !ITEMCONSTS_H
